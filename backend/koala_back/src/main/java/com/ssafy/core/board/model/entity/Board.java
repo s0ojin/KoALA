09a13dda6b4b77fd23f44fb85483e3a0d7810173
LeeSpect <wewqwew153@gmail.com>
2024-07-19 12:55:33 +0900

    Feat: boards, lectures, sentence 등 dto 일부 구현

--- backend/koala_back/src/main/java/com/ssafy/core/board/model/entity/Board.java
+++ backend/koala_back/src/main/java/com/ssafy/core/board/model/entity/Board.java
@@ -1,5 +1,38 @@
 package com.ssafy.core.board.model.entity;
 
+import com.ssafy.core.user.model.entity.User;
+import jakarta.persistence.*;
 
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "board_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "board_title")
+    private String title;
+
+    @Column(name = "board_content")
+    private String content;
+
+    @Column(name = "board_created_at")
+    private Date boardCreatedAt;
+
+    @Column(name = "board_modified_at")
+    private Date boardModifiedAt;
+
+    @Column(name = "hit")
+    private Integer hit;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<BoardComment> boardComments;
 }
--- backend/koala_back/src/main/java/com/ssafy/core/board/model/entity/BoardComment.java
+++ backend/koala_back/src/main/java/com/ssafy/core/board/model/entity/BoardComment.java
@@ -1,4 +1,34 @@
 package com.ssafy.core.board.model.entity;
 
+import com.ssafy.core.user.model.entity.User;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class BoardComment {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "comment_content")
+    private String commentContent;
+
+    @Column(name = "comment_created_at")
+    private Date commentCreatedAt;
+
+    @Column(name = "comment_modified_at")
+    private Date commentModifiedAt;
+
+
 }
