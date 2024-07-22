3e77a7ac77b92e61ae7a5f1ad7e4c4917a262b69
ssyoung102 <ssyoung102@gmail.com>
2024-07-23 02:01:16 +0900

    Feat: JWT 관련 config 파일

--- backend/koala_back/src/main/java/com/ssafy/domain/user/model/entity/User.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/model/entity/User.java
@@ -9,7 +9,10 @@ import com.ssafy.domain.sentence.model.entity.Sentence;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -17,7 +20,7 @@ import java.util.List;
 @Setter
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -67,4 +70,13 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<BoardComment> boardComments;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
 }
