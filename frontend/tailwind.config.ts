1e30e1852690df7ed50e2d3d03fcfe9d3d5c208d
yukyung <kyg0954@naver.com>
2024-07-22 16:44:25 +0900

    Chore: tailwind setting

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        'primary-50': '#EEEDFF',
+        'primary-100': '#C7C4FF',
+        'primary-200': '#A09BFF',
+        'primary-300': '#7A72FF',
+        'primary-400': '#4F46F2',
+        'primary-500': '#3A32D0',
+        'primary-600': '#2921AE',
+        'primary-700': '#1A148C',
+        'primary-800': '#0F0A6A',
+        'primary-900': '#070348',
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primary-50': '#EEEDFF',
+        'primary-100': '#C7C4FF',
+        'primary-200': '#A09BFF',
+        'primary-300': '#7A72FF',
+        'primary-400': '#4F46F2',
+        'primary-500': '#3A32D0',
+        'primary-600': '#2921AE',
+        'primary-700': '#1A148C',
+        'primary-800': '#0F0A6A',
+        'primary-900': '#070348',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
