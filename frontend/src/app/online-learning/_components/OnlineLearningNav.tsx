d1bcbf00fe3b711bff1b77ba7b72e32abf5dbbc1
unknown <one_pst@naver.com>
2024-07-23 17:18:35 +0900

    Design: 화상강의방 레이아웃

---
+++ frontend/src/app/online-learning/_components/OnlineLearningNav.tsx
@@ -0,0 +1,3 @@
+export default function OnlineLearningNav() {
+  return <div className="fixed right-0 w-20 h-full bg-white">네브바임^^</div>
+}
---
+++ frontend/src/app/online-learning/lecture/[id]/page.tsx
@@ -0,0 +1,10 @@
+export default function OnlineLearningRoom() {
+  return (
+    <div className="flex gap-20 items-center h-screen">
+      <div className="w-full h-[80%] ml-20 bg-gray-400 rounded-[3rem] overflow-hidden">
+        <img src="https://images.unsplash.com/photo-1544168190-79c17527004f?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      </div>
+      <div className="bg-black w-[60rem] h-screen rounded-tl-[3rem]"></div>
+    </div>
+  )
+}
