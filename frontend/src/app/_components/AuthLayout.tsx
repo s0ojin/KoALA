09a7aa6284a4ed08aea4af9cbf5a9a594eb5f520
s0ojin <onepst@hanyang.ac.kr>
2024-07-23 13:34:53 +0900

    Refactor: 회원가입 배경 UI 분리 - 로그인페이지와 공통으로 사용하는 AuthLayout으로 분리

---
+++ frontend/src/app/_components/AuthLayout.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image'
+
+interface AuthLayoutProps {
+  children: React.ReactNode
+}
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+  return (
+    <div>
+      {children}
+      <Image
+        src="/images/cloud.svg"
+        alt="cloud"
+        width={240}
+        height={240}
+        priority
+        className="absolute bottom-10 left-10"
+      />
+      <Image
+        src="/images/cloud.svg"
+        alt="cloud"
+        width={200}
+        height={200}
+        priority
+        className="absolute top-40 right-0 translate-x-10"
+      />
+      <Image
+        src="/images/cloud.svg"
+        alt="cloud"
+        width={150}
+        height={150}
+        priority
+        className="absolute top-10 left-2/3 translate-x-10"
+      />
+      <Image
+        src="/images/koala-sleep.png"
+        alt="koala-sleep"
+        width={100}
+        height={100}
+        priority
+        className="absolute top-2 left-2/3 translate-x-14"
+      />
+      <Image
+        src="/images/koala-tree.png"
+        alt="koala-tree"
+        width={460}
+        height={460}
+        priority
+        className="absolute bottom-0 translate-y-2 left-28"
+      />
+    </div>
+  )
+}
