e193d1eb22045cd0acd3b8f94326224e7dcd63e9
tjdgus2308 <tjdgus2308@gmail.com>
2024-07-23 18:02:43 +0900

    Design: 화상  메인 페이지 UI 제작

---
+++ frontend/src/app/online-learning/_components/header.tsx
@@ -0,0 +1,12 @@
+export default function Header ({...props}) {
+  return (
+    <header className="mt-28">
+      <h1 className="text-center text-3xl text-gray-800 font-bold">
+      {props.title}
+      </h1>
+      <p className="text-center mt-2 text-xl text-gray-800 font=medium">
+        {props.description}
+      </p>
+    </header>
+  )
+}
--- frontend/src/app/online-learning/page.tsx
+++ frontend/src/app/online-learning/page.tsx
@@ -1,7 +1,15 @@
+import Header from './_components/header'
+// import Cards from "./_components/cards"
+import CardSlide from './_components/carousel/CardSlide'
+
 export default function Main() {
   return (
-    <div>
-      <h2>Main Page.</h2>
-    </div>
+    <>
+      <Header
+        title="화상 수업"
+        description="한국인 강사와 함께 한국어를 즐겁게 배워보아요!"
+      ></Header>
+      <CardSlide />
+    </>
   )
 }
