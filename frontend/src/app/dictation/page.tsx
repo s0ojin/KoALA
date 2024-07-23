fdede44211aff13a61a09f2126a7d3a8f05dc8f4
yukyung <kyg0954@naver.com>
2024-07-23 17:05:23 +0900

    Feat: 받아쓰기 문제 UI
    - 받아쓰기 문제 UI 제작
    - 받아쓰기 문제 토글 UI 제작

---
+++ frontend/src/app/dictation/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import DictationQuestion from '@/app/dictation/_components/DictationQuestion'
+
+export default function Dictation() {
+  return (
+    <div className="w-full h-full">
+      <DictationQuestion />
+    </div>
+  )
+}
