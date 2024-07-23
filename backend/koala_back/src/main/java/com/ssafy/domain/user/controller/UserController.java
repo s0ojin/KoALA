aa3ea499591a43872ac2ab65f4beaa70dca40032
ssyoung102 <ssyoung102@gmail.com>
2024-07-23 16:40:12 +0900

    Feat: signIn 기능 구현

--- backend/koala_back/src/main/java/com/ssafy/domain/user/controller/UserController.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.ssafy.domain.user.controller;
 
+import com.ssafy.domain.user.model.dto.request.SignInDto;
 import com.ssafy.domain.user.model.dto.request.UserAddRequest;
 import com.ssafy.domain.user.service.UserService;
 import com.ssafy.domain.user.service.UserServiceImpl;
+import com.ssafy.global.auth.jwt.dto.JwtToken;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -21,11 +25,22 @@ public class UserController {
     // 회원가입 성공하면 {
     //	"message": "회원가입 성공!"
     //} 반환
+
     @PostMapping
     public ResponseEntity<?> signUp(@RequestBody JSONObject jsonObject) {
         UserAddRequest userAddRequest = new UserAddRequest(jsonObject);
         userService.signUp(userAddRequest);
         return ResponseEntity.ok().body("회원가입 성공!");
+    }
+
+    @PostMapping("/login")
+    public JwtToken signIn(@RequestBody SignInDto signInDto){
+        String loginId = signInDto.getLoginId();
+        String password = signInDto.getPassword();
+        JwtToken jwtToken = userService.signIn(loginId, password);
+        log.info("request loginId: {}, password: {}", loginId, password);
+        log.info("jwtToken accessToken: {}, refreshToken: {}", jwtToken.getAccessToken(), jwtToken.getRefreshToken());
+        return jwtToken;
     }
 
 }
