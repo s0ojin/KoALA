15fc67b5280302c93a65e997ccf77c0d5b0b36ec
ssyoung102 <ssyoung102@gmail.com>
2024-07-23 14:50:07 +0900

    Feat: UserService에 jwt 관련 메소드 설정

---
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ssafy.domain.user.service;
+
+import com.ssafy.domain.user.repository.UserRepository;
+import com.ssafy.global.auth.jwt.JwtTokenProvider;
+import com.ssafy.global.auth.jwt.dto.JwtToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Transactional
+    @Override
+    public JwtToken signIn(String loginId, String password) {
+        // loginId + password 를 기반으로 Authentication 객체 생성
+        // 이때 authentication은 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginId, password);
+
+        // 실제 검증. authenticate() 메서드를 통해 요청된 User 에 대한 검증 진행
+        // authenticate 메서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 인증 정보를 기반으로 JWT 토큰 생성
+        JwtToken jwtToken = jwtTokenProvider.generateToken(authentication);
+        return jwtToken;
+    }
+}
