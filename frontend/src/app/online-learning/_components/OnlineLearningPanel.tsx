73dab3b7172281ea8123200244313b93bd9a1f05
박수진 <one_pst@naver.com>
2024-07-24 17:35:16 +0900

    Feat: 화상채팅 패널 공지 UI

--- frontend/src/app/online-learning/_components/OnlineLearningPanel.tsx
+++ frontend/src/app/online-learning/_components/OnlineLearningPanel.tsx
@@ -1,9 +1,31 @@
-export default function OnlineLearningPanel({ children, tabId }) {
+'use client'
+
+import {
+  TabId,
+  tabList,
+} from '@/app/online-learning/_components/OnlineLearningTabList'
+import NotiIcon from '/public/icons/notification.svg'
+
+interface OnlineLearningPanelProps {
+  children: React.ReactNode
+  tabId: TabId
+}
+
+export default function OnlineLearningPanel({
+  children,
+  tabId,
+}: OnlineLearningPanelProps) {
+  const activeTab = tabList.find((tab) => tab.id == tabId)
   return (
     <div className="bg-white w-[60rem] h-screen rounded-tl-[3rem] overflow-hidden shadow-lg">
-      <div className="h-28 shadow-md">
-        <h3>강의노트</h3>
+      <div className="h-28 shadow-md p-6 flex flex-col justify-center">
+        <h2 className="font-medium text-xl">{activeTab?.label}</h2>
+        <div className="flex items-center gap-1">
+          <NotiIcon width="24" height="24" />
+          <p>{activeTab?.description}</p>
+        </div>
       </div>
+      {children}
     </div>
   )
 }
--- frontend/src/app/online-learning/_components/OnlineLearningTabList.tsx
+++ frontend/src/app/online-learning/_components/OnlineLearningTabList.tsx
@@ -5,21 +5,45 @@ import NoteIcon from '/public/icons/notebook.svg'
 import WriteIcon from '/public/icons/edit.svg'
 import { useState } from 'react'
 
-const tabs = [
-  { id: 'lecture-note', label: '강의노트', icon: <WriteIcon /> },
-  { id: 'lecture-handout', label: '수업자료', icon: <NoteIcon /> },
-  { id: 'lecture-chat', label: '채팅', icon: <ChatIcon /> },
+export type TabId = 'lecture-note' | 'lecture-handout' | 'lecture-chat'
+
+interface Tab {
+  id: TabId
+  label: string
+  icon: React.ReactNode
+  description: string
+}
+
+export const tabList: Tab[] = [
+  {
+    id: 'lecture-note',
+    label: '강의노트',
+    icon: <WriteIcon />,
+    description: '수업을 들으면서 중요한 부분을 기록해요!',
+  },
+  {
+    id: 'lecture-handout',
+    label: '수업자료',
+    icon: <NoteIcon />,
+    description: '선생님이 제공한 문장 중에서 복습하고 싶은 문장을 저장하세요!',
+  },
+  {
+    id: 'lecture-chat',
+    label: '채팅',
+    icon: <ChatIcon />,
+    description: '수업과 관련 없는 내용은 피해주세요!',
+  },
 ]
 
 export default function OnlineLearningTabList() {
-  const [tabIndex, setTabIndex] = useState(tabs[0].id)
+  const [activeTab, setActiveTab] = useState(tabList[0].id)
   return (
     <div className="fixed py-6 right-0 w-20 h-full bg-white flex flex-col items-center gap-2">
-      {tabs.map((tab) => (
+      {tabList.map((tab) => (
         <button
-          onClick={() => setTabIndex(tab.id)}
+          onClick={() => setActiveTab(tab.id)}
           key={tab.id}
-          className={`flex flex-col items-center gap-1 w-full py-2 text-primary-400 hover:font-medium ${tabIndex === tab.id && 'bg-primary-400 text-white'}`}
+          className={`flex flex-col items-center gap-1 w-full py-2 text-primary-400 hover:font-medium ${activeTab === tab.id && 'bg-primary-400 text-white'}`}
         >
           <div className="w-8">{tab.icon}</div>
           <p className="font-medium">{tab.label}</p>
