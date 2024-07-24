c996015afe1c798d61f42fd92b160234a0a559d9
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-07-25 00:40:43 +0900

    Fix: 네비게이션 바 위치 및 카드 요소 위치 조정

--- frontend/src/app/online-learning/_components/Card.tsx
+++ frontend/src/app/online-learning/_components/Card.tsx
@@ -1,31 +1,35 @@
-'use client'
-
 import Image from 'next/image'
 import Link from 'next/link'
 import Photo from '/public/images/logo.svg'
-import { useState } from 'react'
 
 export default function Card ({...props}) {
 
   if (props.variant === 'Back') {
     return (
-      <div className="bg-white rounded-tl-3xl overflow-hidden shadow-mg">
-        {/* <Image src={Photo} alt="Lecture Card" className='w-full ' fill/> */}
-        <div className='bg-gray-100 rounded-tl-3xl mt-5 mx-5 pt-8 px-8'>
-          <div className="text-gray-700 font-bold text-xl text-center mb-2">
+      <div className="relative bg-white w-full h-full rounded-tl-3xl overflow-hidden shadow-mg">
+        <div className='absolute w-full h-full'>
+          <img 
+            className='w-full h-full object-cover blur-2xl' 
+            src="https://res.cloudinary.com/amazingtalker/image/upload/c_fill,f_auto,q_auto:best,w_800/pages/apply-to-teach/job-description/feature-price.jpg"
+            alt=""
+          />
+          {/* <Image src={Photo} alt="Lecture Card" className='w-full h-full object-cover blur-2xl' layout='fill'/> */}
+        </div>
+        <div className='absolute bg-white h-full rounded-tl-3xl mt-5 mx-5 pt-8 px-8'>
+          <div className="text-gray-700 font-bold text-xl text-center mb-[1.5rem]">
             {props.lecture}
           </div>
           <div>
-            <p className="text-gray-700 text-base">
+            <p className="text-gray-700 text-base h-[11rem]">
               {props.description}
             </p>
           </div>
-          <div className='flex text-primary-900 text-sm'>
+          <div className='flex text-primary-900 text-sm mb-[1.25rem]'>
                 <p className='mr-2.5 font-medium'>수업일정</p>
                 <p>{props.schedule}</p>
           </div>
           <Link href={`online-learning/${props.pagelink}`} className='m-auto'>
-            <button className='bg-primary-400 max-w-56 max-h-10 rounded-3xl text-white font-bold'>
+            <button className='bg-primary-400 w-[14rem] h-[2.5rem] rounded-3xl text-white font-bold'>
               강의 입장하기
             </button>
           </Link>
@@ -36,13 +40,18 @@ export default function Card ({...props}) {
     return (
       <div className="bg-white rounded-tr-3xl overflow-hidden shadow-lg">
         <div className="w-full h-80">
-          <Image src={Photo} alt="Lecture Card" className='w-full'/>
+          {/* <Image src={Photo} alt="Lecture Card" className='w-full h-full'/> */}
+          <img 
+            className='w-full h-full' 
+            src="https://res.cloudinary.com/amazingtalker/image/upload/c_fill,f_auto,q_auto:best,w_800/pages/apply-to-teach/job-description/feature-price.jpg"
+            alt=""
+          />
         </div>
         <div className="px-6 py-4">
-          <div className="text-gray-700 font-bold text-xl text-center mb-2">
+          <div className="text-primary-900 font-medium text-xl text-center mb-2">
             {props.lecture}
           </div>
-          <p className="text-gray-700 text-base text-center">
+          <p className="text-primary-900 text-base text-center font-regular">
             {props.teacher}
           </p>
         </div>
--- frontend/src/app/online-learning/_components/carousel/CardSlide.tsx
+++ frontend/src/app/online-learning/_components/carousel/CardSlide.tsx
@@ -3,29 +3,26 @@
 'use client'
 
 import Image from 'next/image';
-import Logo from '/public/images/logo.svg'
 import { ReactElement, useEffect, useRef, useState } from 'react';
-import SwiperCore from 'swiper';
-import { Virtual, Navigation, Pagination, Autoplay, Scrollbar } from 'swiper/modules'
+import { Virtual, Autoplay } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import Card from '@/app/online-learning/_components/Card'
 import CardFlip from '@/app/online-learning/_components/carousel/CardFlip';
 // Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/navigation'
 
 const cardList = [
-  { lecture: '안녕1',
-    teacher: '빡빡이 아저씨야.',
+  { lecture: '식당에 방문 했을 때 II',
+    teacher: '고동연 선생님',
     description: '식당에 들어설 때부터 마주칠 수 있는 다양한 상황에서 나올 수 있는 대화를 다룹니다. <식당에 방문 했을 때 I> 수업을 먼저 수강해야합니다.',
     schedule: '월 수 금 15:00 ~ 17:00',
-    pagelink: '/1'
+    pagelink: 'lecture/1'
   },
   { lecture: '안녕2',
     teacher: '빡빡이 아저씨야.',
     description: '식당에 들어설 때부터 마주칠 수 있는 다양한 상황에서 나올 수 있는 대화를 다룹니다. <식당에 방문 했을 때 I> 수업을 먼저 수강해야합니다.',
     schedule: '월 수 금 15:00 ~ 17:00',
-    pagelink: '/1'
+    pagelink: '/2'
   },
   { lecture: '안녕3',
     teacher: '빡빡이 아저씨야.',
@@ -45,63 +42,45 @@ const cardList = [
     schedule: '월 수 금 15:00 ~ 17:00',
     pagelink: '/1'
   },
-  { lecture: '안녕1',
-    teacher: '빡빡이 아저씨야.',
-    description: '식당에 들어설 때부터 마주칠 수 있는 다양한 상황에서 나올 수 있는 대화를 다룹니다. <식당에 방문 했을 때 I> 수업을 먼저 수강해야합니다.',
-    schedule: '월 수 금 15:00 ~ 17:00',
-    pagelink: '/1'
-  },
-  { lecture: '안녕1',
-    teacher: '빡빡이 아저씨야.',
-    description: '식당에 들어설 때부터 마주칠 수 있는 다양한 상황에서 나올 수 있는 대화를 다룹니다. <식당에 방문 했을 때 I> 수업을 먼저 수강해야합니다.',
-    schedule: '월 수 금 15:00 ~ 17:00',
-    pagelink: '/1'
-  },
-
 ]
 
 export default function CardSlide() {
-  const [swiperRef, setSwiperRef] = useState(null)
-  SwiperCore.use([Navigation])
-  // const appendNumber = useRef(cardData.length)
-  // const prependNumber = useRef(1)
   const [slides, setSlides] = useState(
     cardList.map((data, index) => <CardFlip key={index} {...data} />)
   )
 
-  // function prepend ():void {
-  //   setSlides([
-  //     `Slide ${prependNumber.current - 2}`,
-  //     `Slide ${prependNumber.current - 1}`,
-  //     ...slides,
-  //   ])
-  //   prependNumber.current = prependNumber.current - 2
-  //   swiperRef.slideTo(swiperRef.activeIndex + 2, 0)
-  // }
+  // 1. 연결을 위한 state 만들기
+  const [swiperIndex, setSwiperIndex] = useState(0); // -> 페이지네이션용
+  const [swiper, setSwiper] = useState<SwiperClass>(); // -> 슬라이드용
 
-  // const append = () => {
-  //   setSlides([...slides, 'Slide ' + ++appendNumber.current])
-  // }
-
-  // const slideTo = (index) => {
-  //   swiperRef.slideTo(index - 1, 0)
-  // }
+  // 2. 슬라이드 이벤트핸들러
+  const handlePrev = () => {
+      swiper?.slidePrev()
+  }
+  const handleNext = () => {
+    swiper?.slideNext()
+  }
 
   return (
-    <div className='mt-24 w-5/6 m-auto'>
+    <div className='flex mt-24 w-[80rem] min-h-[26rem] mx-auto'>
+      <div className='m-auto'>
+        <button onClick={handlePrev}>
+          <img src="https://w7.pngwing.com/pngs/551/108/png-transparent-arrow-illustration-arrow-icon-right-arrow-angle-web-design-internet-thumbnail.png" className="w-[2.5rem] rotate-180" alt="" />
+        </button>
+      </div>
       <Swiper
-        modules={[Virtual, Navigation, Autoplay]}
-        // onSwiper={setSwiperRef}
+        className='w-[70rem] m-auto'
+        modules={[Virtual, Autoplay]}
+        onActiveIndexChange={(e)=>setSwiperIndex(e.realIndex)}
+        onSwiper={(e) => {setSwiper(e)}}
         slidesPerView={3}
         centeredSlides={true}
-        spaceBetween={20}
+        spaceBetween={50}
         loop={true}
-        navigation={true}
         autoplay={{
           delay: 5000, // 5초마다 자동 재생
           disableOnInteraction: false // 사용자 상호 작용 후에도 자동 재생 유지
         }}
-        virtual
       >
         {slides.map((slideContent, index) => (
           <SwiperSlide key={slideContent} virtualIndex={index}>
@@ -109,6 +88,11 @@ export default function CardSlide() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className='m-auto'>
+        <button onClick={handleNext}>
+          <img src="https://w7.pngwing.com/pngs/551/108/png-transparent-arrow-illustration-arrow-icon-right-arrow-angle-web-design-internet-thumbnail.png" className="w-[2.5rem]" alt="" />
+        </button>
+      </div>
     </div>
   )
 }
