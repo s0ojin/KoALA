4f03b682a59a97b711077f66613d86a08d031e0f
s0ojin <onepst@hanyang.ac.kr>
2024-07-24 14:18:38 +0900

    Feat: 화상수업 네브바 제작

--- frontend/src/app/online-learning/_components/OnlineLearningNav.tsx
+++ frontend/src/app/online-learning/_components/OnlineLearningNav.tsx
@@ -1,3 +1,30 @@
+'use client'
+
+import ChatIcon from '/public/icons/chat.svg'
+import NoteIcon from '/public/icons/notebook.svg'
+import ResourceIcon from '/public/icons/edit.svg'
+import { useState } from 'react'
+
+const tabs = [
+  { id: 0, label: '강의노트', icon: <ResourceIcon /> },
+  { id: 1, label: '수업자료', icon: <NoteIcon /> },
+  { id: 2, label: '채팅', icon: <ChatIcon /> },
+]
+
 export default function OnlineLearningNav() {
-  return <div className="fixed right-0 w-20 h-full bg-white">네브바임^^</div>
+  const [tabIndex, setTabIndex] = useState(0)
+  return (
+    <div className="fixed py-6 right-0 w-20 h-full bg-white flex flex-col items-center gap-2">
+      {tabs.map((tab) => (
+        <button
+          onClick={() => setTabIndex(tab.id)}
+          key={tab.id}
+          className={`flex flex-col items-center gap-1 w-full py-2 text-primary-400 hover:font-medium ${tabIndex === tab.id && 'bg-primary-400 text-white'}`}
+        >
+          <div className="w-8">{tab.icon}</div>
+          <p className="font-medium">{tab.label}</p>
+        </button>
+      ))}
+    </div>
+  )
 }
