85f388758a39195511220fafa71fe7d9fb06a1ee
tjdgus2308 <tjdgus2308@gmail.com>
2024-07-24 18:00:31 +0900

    Feat: 카드 플립 애니메이션 추가

--- frontend/src/app/online-learning/_components/Card.tsx
+++ frontend/src/app/online-learning/_components/Card.tsx
@@ -1,20 +1,53 @@
+'use client'
+
 import Image from 'next/image'
+import Link from 'next/link'
 import Photo from '/public/images/logo.svg'
+import { useState } from 'react'
 
 export default function Card ({...props}) {
-  return (
-    <div className="bg-white max-w-80 rounded-[24px] overflow-hidden shadow-lg">
-      <div className="w-full h-80">
-        <Image src={Photo} alt="Lecture Card" className='w-full'/>
-      </div>
-      <div className="px-6 py-4">
-        <div className="text-gray-700 font-bold text-xl text-center mb-2">
-          {props.lecture}
+
+  if (props.variant === 'Back') {
+    return (
+      <div className="bg-white rounded-tl-3xl overflow-hidden shadow-mg">
+        {/* <Image src={Photo} alt="Lecture Card" className='w-full ' fill/> */}
+        <div className='bg-gray-100 rounded-tl-3xl mt-5 mx-5 pt-8 px-8'>
+          <div className="text-gray-700 font-bold text-xl text-center mb-2">
+            {props.lecture}
+          </div>
+          <div>
+            <p className="text-gray-700 text-base">
+              {props.description}
+            </p>
+          </div>
+          <div className='flex text-primary-900 text-sm'>
+                <p className='mr-2.5 font-medium'>수업일정</p>
+                <p>{props.schedule}</p>
+          </div>
+          <Link href={`online-learning/${props.pagelink}`} className='m-auto'>
+            <button className='bg-primary-400 max-w-56 max-h-10 rounded-3xl text-white font-bold'>
+              강의 입장하기
+            </button>
+          </Link>
         </div>
-        <p className="text-gray-700 text-base text-center">
-          {props.teacher}
-        </p>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="bg-white rounded-tr-3xl overflow-hidden shadow-lg">
+        <div className="w-full h-80">
+          <Image src={Photo} alt="Lecture Card" className='w-full'/>
+        </div>
+        <div className="px-6 py-4">
+          <div className="text-gray-700 font-bold text-xl text-center mb-2">
+            {props.lecture}
+          </div>
+          <p className="text-gray-700 text-base text-center">
+            {props.teacher}
+          </p>
+        </div>
+      </div>
+    )
+  }
+
 }
