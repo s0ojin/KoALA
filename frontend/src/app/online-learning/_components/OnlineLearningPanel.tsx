5c8947a87c58a392ef56fcba4bcccb2fb81a1910
unknown <one_pst@naver.com>
2024-07-24 15:01:26 +0900

    Refactor: 화상수업 레이아웃 및 파일구조 변경 - OnlineLearningNav를 OnlineLearningTabList로 변경 - online-learning폴더의 layout.tsx 삭제

---
+++ frontend/src/app/online-learning/_components/OnlineLearningPanel.tsx
@@ -0,0 +1,9 @@
+export default function OnlineLearningPanel({ children, tabId }) {
+  return (
+    <div className="bg-white w-[60rem] h-screen rounded-tl-[3rem] overflow-hidden shadow-lg">
+      <div className="h-28 shadow-md">
+        <h3>강의노트</h3>
+      </div>
+    </div>
+  )
+}
