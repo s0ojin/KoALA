c5cd4d7a77a8c6f36930f101b93e2f489a35f0d6
yukyung <kyg0954@naver.com>
2024-07-24 17:40:49 +0900

    Feat: 받아쓰기 원고지 UI

--- frontend/src/app/dictation/page.tsx
+++ frontend/src/app/dictation/page.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import DictationQuestion from '@/app/dictation/_components/DictationQuestion'
+import DictationField from '@/app/dictation/_components/DictationField'
+import Cloud from '/public/images/cloud.svg'
 
 export default function Dictation() {
   return (
-    <div className="w-full h-full">
-      <DictationQuestion />
+    <div className="w-full h-full min-h-screen flex items-center justify-center">
+      <p className="absolute top-[152px] right-[47px]">
+        <Cloud />
+      </p>
+
+      <div className="inline-flex flex-col items-center gap-28 w-auto">
+        <DictationQuestion />
+        <DictationField />
+      </div>
     </div>
   )
 }
