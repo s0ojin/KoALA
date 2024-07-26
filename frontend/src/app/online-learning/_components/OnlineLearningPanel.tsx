ba388085e95775456dd4b8acfb0fa3543c09525f
박수진 <one_pst@naver.com>
2024-07-26 09:21:54 +0900

    Feat: tab state lifting - 탭 상태를 패널컴포넌트와 공유하기 위해 상태 끌어올림

--- frontend/src/app/online-learning/_components/OnlineLearningPanel.tsx
+++ frontend/src/app/online-learning/_components/OnlineLearningPanel.tsx
@@ -1,28 +1,29 @@
 'use client'
 
 import {
-  TabId,
   tabList,
+  TabId,
 } from '@/app/online-learning/_components/OnlineLearningTabList'
 import NotiIcon from '/public/icons/notification.svg'
 
 interface OnlineLearningPanelProps {
+  activeTab: TabId
   children: React.ReactNode
-  tabId: TabId
 }
 
 export default function OnlineLearningPanel({
+  activeTab,
   children,
-  tabId,
 }: OnlineLearningPanelProps) {
-  const activeTab = tabList.find((tab) => tab.id == tabId)
+  const activeTabInfo = tabList.find((tab) => tab.id === activeTab)
+
   return (
-    <div className="bg-white w-[60rem] h-screen rounded-tl-[3rem] overflow-hidden shadow-lg">
-      <div className="h-28 shadow-md p-6 flex flex-col justify-center">
-        <h2 className="font-medium text-xl">{activeTab?.label}</h2>
+    <div className="bg-white w-[52rem] h-screen rounded-tl-[3rem] overflow-hidden shadow-lg mr-20">
+      <div className="h-28 shadow-md p-6 flex flex-col justify-center gap-1">
+        <h2 className="font-medium text-xl">{activeTabInfo?.label}</h2>
         <div className="flex items-center gap-1">
           <NotiIcon width="24" height="24" />
-          <p>{activeTab?.description}</p>
+          <p className="mx-2">{activeTabInfo?.description}</p>
         </div>
       </div>
       {children}
