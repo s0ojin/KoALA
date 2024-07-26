24f6fb36e6f8f29e2928a9cbcaf2b355d57c0ccb
ssyoung102 <ssyoung102@gmail.com>
2024-07-26 16:21:09 +0900

    Feat: 받아쓰기 채점 Dto 생성

---
+++ backend/koala_back/src/main/java/com/ssafy/domain/sentence/model/dto/request/SentenceTestRequest.java
@@ -0,0 +1,9 @@
+package com.ssafy.domain.sentence.model.dto.request;
+
+import com.ssafy.domain.sentence.model.entity.Sentence;
+
+public class SentenceTestRequest {
+    private Long sentenceId;
+    String userSentence;
+    boolean isToggled;
+}
