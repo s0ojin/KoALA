066e801befcdddc3a2cc7114bd978c51c396f057
wewqwew153 <wewqwew153@gmail.com>
2024-07-27 22:55:58 +0900

    Feat: Koala 조회 Service, Controller 구현

--- backend/koala_back/src/main/java/com/ssafy/domain/koala/service/KoalaService.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/koala/service/KoalaService.java
@@ -1,21 +1,7 @@
 package com.ssafy.domain.koala.service;
 
-import com.ssafy.domain.koala.model.entity.Koala;
-import com.ssafy.domain.koala.repository.KoalaRepository;
-import com.ssafy.domain.user.model.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.ssafy.domain.koala.model.dto.response.KoalaResponse;
 
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class KoalaService {
-
-    private final KoalaRepository koalaRepository;
-
-    public void createKoala(User user) {
-        Koala koala = new Koala(user, "코알라", 1, 0, 1);
-        koalaRepository.save(koala);
-    }
+public interface KoalaService {
+    public KoalaResponse findKoala();
 }
