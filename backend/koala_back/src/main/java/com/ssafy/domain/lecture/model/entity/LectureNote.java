c8ff02125c59c9efe8d6692a94196c3d469a82ba
wewqwew153 <wewqwew153@gmail.com>
2024-07-28 00:58:31 +0900

    Refactor: Lecture 객체를 JPA에 맞게 수정 및 자바 컨벤션 적용

--- backend/koala_back/src/main/java/com/ssafy/domain/lecture/model/entity/LectureNote.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/lecture/model/entity/LectureNote.java
@@ -1,44 +1,53 @@
 package com.ssafy.domain.lecture.model.entity;
 
+import static jakarta.persistence.FetchType.*;
+import static lombok.AccessLevel.*;
+
+import java.time.LocalDateTime;
+
 import com.ssafy.domain.user.model.entity.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.Date;
-
-import static jakarta.persistence.FetchType.LAZY;
-import static lombok.AccessLevel.PROTECTED;
-
 @Entity
 @Getter
+@Builder
 @Table(name = "lecture_notes")
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PROTECTED)
 public class LectureNote {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "note_id", nullable = false)
-    private Long noteId;
+	@Id
+	@GeneratedValue
+	@Column(name = "note_id", nullable = false)
+	private Long noteId;
 
-    @Column(name = "note_title", nullable = false)
-    private String noteTitle;
+	@Column(name = "note_title", nullable = false)
+	private String noteTitle;
 
-    @Column(name = "note_content", nullable = false)
-    private String noteContent;
+	@Column(name = "note_content", nullable = false)
+	private String noteContent;
 
-    @Column(name = "note_created_at", nullable = false)
-    private LocalDateTime noteCreatedAt = LocalDateTime.now();
+	@Builder.Default
+	@Column(name = "note_created_at")
+	private LocalDateTime noteCreatedAt = LocalDateTime.now();
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "lecture_id", nullable = false)
-    private Lecture lecture;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "lecture_id", nullable = false)
+	private Lecture lecture;
 
 }
--- backend/koala_back/src/main/java/com/ssafy/domain/lecture/model/entity/RegisteredLectureId.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/lecture/model/entity/RegisteredLectureId.java
@@ -15,20 +15,22 @@ import static lombok.AccessLevel.*;
 @AllArgsConstructor
 public class RegisteredLectureId implements Serializable {
 
-    private Long userId;
-    private Long lectureId;
+	private Long userId;
+	private Long lectureId;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof RegisteredLectureId)) return false;
-        RegisteredLectureId that = (RegisteredLectureId) o;
-        return Objects.equals(userId, that.userId) && Objects.equals(lectureId, that.lectureId);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof RegisteredLectureId))
+			return false;
+		RegisteredLectureId that = (RegisteredLectureId)o;
+		return Objects.equals(userId, that.userId) && Objects.equals(lectureId, that.lectureId);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(userId, lectureId);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(userId, lectureId);
+	}
 
 }
