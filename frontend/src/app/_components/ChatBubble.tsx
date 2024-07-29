e31def8169f0215e0fd0632f7003269207f0e807
박수진 <one_pst@naver.com>
2024-07-29 10:42:52 +0900

    Feat: ChatBubble 컴포넌트

---
+++ frontend/src/app/_components/ChatBubble.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+
+interface ChatBubbleProps {
+  isMine: boolean
+  message: string
+  senderName?: string
+  senderProfile?: string
+  timeStamp?: string
+}
+
+export default function ChatBubble({
+  isMine,
+  message,
+  senderName,
+  senderProfile,
+  timeStamp,
+}: ChatBubbleProps) {
+  const getFormattedTime = (timestamp: string) => {
+    const date = new Date(timestamp)
+    return date.toLocaleTimeString('ko-KR', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    })
+  }
+
+  return (
+    <div className={`flex gap-2 ${isMine && 'self-end flex-row-reverse'}`}>
+      {!isMine && senderProfile && (
+        <div className="profile w-10 h-10 rounded-full bg-black">
+          <Image src={senderProfile} alt="profile" width={16} height={16} />
+        </div>
+      )}
+      <div>
+        {!isMine && <p className="text-sm mb-1">{senderName}</p>}
+        <div
+          className={`w-full px-4 py-3 ${isMine ? 'bg-primary-400 text-white rounded-full rounded-tr-none' : 'bg-gray-100 rounded-full rounded-tl-none'}`}
+        >
+          <p>{message}</p>
+        </div>
+      </div>
+      <p className="self-end text-sm text-gray-500">
+        {timeStamp && getFormattedTime(timeStamp)}
+      </p>
+    </div>
+  )
+}
