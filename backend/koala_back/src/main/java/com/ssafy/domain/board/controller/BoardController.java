80a005663f90e54a47b857794c754a3e4904e599
LeeSpect <wewqwew153@gmail.com>
2024-07-29 15:57:24 +0900

    Feat: Board Controller 생성

---
+++ backend/koala_back/src/main/java/com/ssafy/domain/board/controller/BoardController.java
@@ -0,0 +1,20 @@
+package com.ssafy.domain.board.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.domain.board.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/boards")
+public class BoardController {
+
+	private final BoardService boardService;
+
+	
+}
