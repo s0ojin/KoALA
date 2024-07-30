dfb8721f2fd97b007dbd591442d59d186cc75fa2
ssyoung102 <ssyoung102@gmail.com>
2024-07-30 16:11:41 +0900

    Feat: AI 회화 추가

---
+++ backend/koala_back/src/main/java/com/ssafy/domain/chat/dto/Message.java
@@ -0,0 +1,19 @@
+package com.ssafy.domain.chat.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+    private String role;
+    private String content;
+
+    @JsonCreator
+    public Message(@JsonProperty("role") String role, @JsonProperty("content") String content) {
+        this.role = role;
+        this.content = content;
+    }
+}
