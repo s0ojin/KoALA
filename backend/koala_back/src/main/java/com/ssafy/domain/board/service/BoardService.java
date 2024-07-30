a0d55225a3f2aec5c8ef6f19ea512a2ebc51b369
LeeSpect <wewqwew153@gmail.com>
2024-07-30 15:12:04 +0900

    Feat: 특정 게시글 상세 조회 시 댓글을 페이징하여 전송

--- backend/koala_back/src/main/java/com/ssafy/domain/board/service/BoardService.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/board/service/BoardService.java
@@ -1,13 +1,18 @@
 package com.ssafy.domain.board.service;
 
+import org.springframework.data.domain.Pageable;
+
 import com.ssafy.domain.board.model.dto.request.BoardCreateRequest;
+import com.ssafy.domain.board.model.dto.response.BoardCreateResponse;
 import com.ssafy.domain.board.model.dto.response.BoardResponse;
 
 public interface BoardService {
 
-	BoardResponse getBoard(Long boardId);
+	BoardResponse getBoard(Long boardId, Pageable pageable);
 
-	BoardResponse createBoard(BoardCreateRequest boardCreateRequest);
+	BoardCreateResponse createBoard(BoardCreateRequest boardCreateRequest);
 
 	void increaseCommentNum(Long boardId);
+
+	void increaseHit(Long boardId);
 }
--- backend/koala_back/src/main/java/com/ssafy/domain/board/service/BoardServiceImpl.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/board/service/BoardServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ssafy.domain.board.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.domain.board.model.dto.request.BoardCreateRequest;
+import com.ssafy.domain.board.model.dto.response.BoardCommentResponse;
+import com.ssafy.domain.board.model.dto.response.BoardCreateResponse;
 import com.ssafy.domain.board.model.dto.response.BoardResponse;
 import com.ssafy.domain.board.model.entity.Board;
 import com.ssafy.domain.board.repository.BoardRepository;
@@ -21,17 +25,19 @@ public class BoardServiceImpl implements BoardService {
 
 	private final UserInfoProvider userInfoProvider;
 	private final BoardRepository boardRepository;
+	private final BoardCommentService boardCommentService;
 
 	@Override
-	public BoardResponse getBoard(Long boardId) {
-		return BoardResponse.toDto(boardRepository.findById(boardId).orElseThrow());
+	public BoardResponse getBoard(Long boardId, Pageable pageable) {
+		Page<BoardCommentResponse> comments = boardCommentService.getCommentsByBoardId(boardId, pageable);
+		return BoardResponse.toDto(boardRepository.findById(boardId).orElseThrow(), comments);
 	}
 
 	@Override
 	@Transactional
-	public BoardResponse createBoard(BoardCreateRequest boardCreateRequest) {
+	public BoardCreateResponse createBoard(BoardCreateRequest boardCreateRequest) {
 		User user = userInfoProvider.getCurrentUser();
-		return BoardResponse.toDto(boardRepository.save(boardCreateRequest.toEntity(user)));
+		return BoardCreateResponse.toDto(boardRepository.save(boardCreateRequest.toEntity(user)));
 	}
 
 	@Override
@@ -39,6 +45,14 @@ public class BoardServiceImpl implements BoardService {
 	public void increaseCommentNum(Long boardId) {
 		Board board = boardRepository.findById(boardId).orElseThrow();
 		board.increaseCommentNum();
+		boardRepository.save(board);
+	}
+
+	@Override
+	@Transactional
+	public void increaseHit(Long boardId) {
+		Board board = boardRepository.findById(boardId).orElseThrow();
+		board.increaseHit();
 		boardRepository.save(board);
 	}
 }
