537cde5982f81bc1f157f9fddf17dd2f88718ca7
KimYuKyung <kyg0954@naver.com>
2024-07-30 09:08:39 +0900

    Feat: 메인페이지 레이아웃 UI

---
+++ frontend/src/app/main/_components/MainLayout.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import { ReactNode } from 'react'
+
+interface MainLayoutProps {
+  children: ReactNode
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div className="w-full h-auto min-h-custom-height overflow-hidden">
+      {children}
+      <Image
+        src="/images/cloud.svg"
+        alt="cloud"
+        width={0}
+        height={0}
+        sizes="100%"
+        priority
+        className="w-[17%] h-auto absolute top-[10%] left-[9%] z-0"
+      />
+      <Image
+        src="/images/cloud.svg"
+        alt="cloud"
+        width={0}
+        height={0}
+        sizes="100%"
+        priority
+        className="w-[13%] h-auto absolute top-[43%] left-[22%] z-0"
+      />
+      <Image
+        src="/images/cloud.svg"
+        alt="cloud"
+        width={0}
+        height={0}
+        sizes="100%"
+        priority
+        className="w-[13%] h-auto absolute top-[21%] left-[47%] z-0"
+      />
+      <Image
+        src="/images/cloud.svg"
+        alt="cloud"
+        width={0}
+        height={0}
+        sizes="100%"
+        priority
+        className="w-[20%] h-auto absolute top-[36%] right-[7%] z-0"
+      />
+    </div>
+  )
+}
