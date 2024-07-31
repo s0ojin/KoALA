86c4872e37de04ec2c5e2402f5fcefe8d5e3d63c
tjdgus2308 <tjdgus2308@gmail.com>
2024-07-31 18:52:07 +0900

    Fix: 배경 레이아웃 수정

--- frontend/src/app/review/page.tsx
+++ frontend/src/app/review/page.tsx
@@ -1,19 +1,21 @@
 import Header from '@/app/_components/Header'
+import Title from '@/app/_components/Title'
 import ReviewArea from '@/app/review/_components/ReviewArea'
 import ReviewLayout from '@/app/review/_components/ReviewLayout'
+import ReviewMenuButtons from '@/app/review/_components/ReviewMenuButtons'
 import Image from 'next/image'
 
 export default function Review() {
-  return(
+  return (
     <ReviewLayout>
       <Header />
-      <div className='mt-[4rem]'>
-        <p className='text-center font-medium text-3xl'>
-          복습하기
-        </p>
-      </div>
-      <div className='flex mt-[1.25rem]'>
+      <Title
+        CourseTitle={'복습하기'}
+        CourseDescription={'반복학습으로 실력 Up!'}
+      />
+      <div className="flex mt-[1.25rem]">
         <ReviewArea />
+        <ReviewMenuButtons />
       </div>
     </ReviewLayout>
   )
