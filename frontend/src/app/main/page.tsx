d3ac273ec3d8416790cab69aee48849b6e1a7e20
yukyung <kyg0954@naver.com>
2024-07-31 16:08:01 +0900

    Refactor: rem 단위 소수점 제거

--- frontend/src/app/main/page.tsx
+++ frontend/src/app/main/page.tsx
@@ -10,7 +10,7 @@ export default function Main() {
     <MainLayout>
       <div className="w-full h-auto min-h-main-height overflow-hidden relative select-none z-10 flex flex-col items-center">
         <Header />
-        <div className="relative w-screen h-full min-h-main-height flex max-w-[93.75rem]">
+        <div className="relative w-screen h-full min-h-main-height flex max-w-[94rem]">
           <div className=" w-2/5 h-1/2 min-h-[32rem] self-end ml-12 mb-8 relative flex items-end justify-center">
             <div className="absolute top-0 left-0">
               <EucalyptusButton eucalyptusCount={10} />
@@ -21,7 +21,7 @@ export default function Main() {
                 width={0}
                 height={0}
                 sizes="100%"
-                className="w-[21.875rem] h-[21.875rem]"
+                className="w-[22rem] h-[22rem]"
                 alt="코알라"
                 draggable="false"
               />
