dda81945bf09bb3dc1198f6d47ea47a001783324
tjdgus2308 <tjdgus2308@gmail.com>
2024-07-31 18:51:40 +0900

    Feat: Sentence 컴포넌트 스타일 추가

--- frontend/src/app/review/_components/Setence.tsx
+++ frontend/src/app/review/_components/Setence.tsx
@@ -1,17 +1,40 @@
-import Image from "next/image"
+'use client'
 
-export default function Sentence () {
+import Image from 'next/image'
+import Play from '/public/icons/play.svg'
+import Pause from '/public/icons/pause.svg'
+import { useState } from 'react'
+
+export default function Sentence() {
+  const [isSelected, setSelected] = useState<Boolean>(false)
+  const [isPlaying, setPlaying] = useState<Boolean>(false)
+
+  const handleChangeSelected = () => {
+    setSelected((isSelected) => !isSelected)
+  }
+
+  const handlePlaying = () => {
+    setPlaying((isPlaying) => !isPlaying)
+  }
+
   return (
-    <div className="flex items-center w-full min-h-[4rem] max-h-[8rem] bg-gray-200 rounded-[4.5rem] mb-[0.75rem] px-[1.25rem]">
-      <p className="grow mr-[1rem] my-[1.25rem]">
-        안녕? 나는 이주형이라고 해 안녕? 나는 이주형이라고 해 안녕? 나는 이주형이라고 해rrrrrrrrrrrrrrrrrrrrrrrrrfasdfasdfasdfsdafasdf
+    <div
+      className={`flex items-center w-full min-h-[4rem] max-h-[8rem] rounded-[4.5rem] mb-[0.75rem] px-[1.25rem] ${isSelected ? 'bg-primary-400' : 'bg-gray-200'}`}
+    >
+      <p
+        onClick={handleChangeSelected}
+        className={`h-full grow pr-[1rem] py-[1.25rem] ${isSelected ? 'text-white' : ''}`}
+      >
+        안녕? 나는 이주형이라고 해 안녕? 나는 이주형이라고 해 안녕? 나는
+        이주형이라고 해rrrrrrrrrrrrrrrrrrrrrrrrrfasdfasdfasdfsdafasdf
       </p>
-      <Image
-        src='/icons/play.svg'
-        alt=''
-        width={24}
-        height={24}
-      />
+      <button onClick={handlePlaying}>
+        {isPlaying ? (
+          <Pause fill={`${isSelected ? 'white' : ''}`} />
+        ) : (
+          <Play fill={`${isSelected ? 'white' : ''}`} />
+        )}
+      </button>
     </div>
   )
 }
