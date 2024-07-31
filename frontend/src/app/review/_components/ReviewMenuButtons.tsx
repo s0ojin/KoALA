ee40ce0e333bd3540c3f56a36217e0c0b160b01f
tjdgus2308 <tjdgus2308@gmail.com>
2024-07-31 18:50:26 +0900

    Add: Button 컴포넌트 추가

---
+++ frontend/src/app/review/_components/ReviewMenuButtons.tsx
@@ -0,0 +1,12 @@
+import ReviewMenuButton from "@/app/review/_components/ReviewMenuButton"
+
+export default function ReviewMenuButtons () {
+  return (
+    <div>
+      <ReviewMenuButton>재생</ReviewMenuButton>
+      <ReviewMenuButton>재생</ReviewMenuButton>
+      <ReviewMenuButton>재생</ReviewMenuButton>
+      <ReviewMenuButton>재생</ReviewMenuButton>
+    </div>
+  )  
+}
