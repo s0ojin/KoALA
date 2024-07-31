af5588cceafd8d40066419f156e11108214beeb9
dongyeon <uuas5866@naver.com>
2024-07-31 16:40:28 +0900

    Chore: WebConfig 추가

--- backend/koala_back/src/main/java/com/ssafy/global/config/SecurityConfig.java
+++ backend/koala_back/src/main/java/com/ssafy/global/config/SecurityConfig.java
@@ -33,9 +33,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequests ->
                                 authorizeRequests
                                         // "/users/register" 경로에 대한 모든 요청을 허용
-                                        .requestMatchers("/users/login").permitAll()
-                                        .requestMatchers("/users/refresh").permitAll()
-                                        .requestMatchers("/users").permitAll()
+                                        .requestMatchers("/api/users/login").permitAll()
+                                        .requestMatchers("/api/users/refresh").permitAll()
+                                        .requestMatchers("/api/users").permitAll()
 //                                        .requestMatchers("/chat").permitAll()
                                         // 다른 모든 요청은 인증을 필요로 함
                                         .anyRequest().authenticated()
