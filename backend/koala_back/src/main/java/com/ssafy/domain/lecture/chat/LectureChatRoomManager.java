28ef0c535ef14a7508980bb95ed107326ef7c97f
ssyoung102 <ssyoung102@gmail.com>
2024-08-02 13:38:02 +0900

    Feat: 강의 채팅방 구현g

---
+++ backend/koala_back/src/main/java/com/ssafy/domain/lecture/chat/LectureChatRoomManager.java
@@ -0,0 +1,17 @@
+package com.ssafy.domain.lecture.chat;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@Getter
+public class LectureChatRoomManager {
+
+	private ConcurrentHashMap<Long, LectureChatRoom> roomSessions = new ConcurrentHashMap<>();
+	private ConcurrentHashMap<String, Long> sessionToRoom = new ConcurrentHashMap<>();
+}
