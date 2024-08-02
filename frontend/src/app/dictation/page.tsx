7cee980687a1d170752cdd94dfb499bd0fde7568
KimYuKyung <kyg0954@naver.com>
2024-08-02 09:09:02 +0900

    Refator: 받아쓰기 메인 페이지에서 Layout 적용

--- frontend/src/app/dictation/page.tsx
+++ frontend/src/app/dictation/page.tsx
@@ -2,19 +2,15 @@
 
 import DictationQuestion from '@/app/dictation/_components/DictationQuestion'
 import DictationField from '@/app/dictation/_components/DictationField'
-import Cloud from '/public/images/cloud.svg'
+import DictationLayout from '@/app/dictation/_components/DictationLayout'
 
 export default function Dictation() {
   return (
-    <div className="w-full h-full min-h-screen flex items-center justify-center">
-      <p className="absolute top-[152px] right-[47px]">
-        <Cloud />
-      </p>
-
+    <DictationLayout>
       <div className="inline-flex flex-col items-center gap-28 w-auto">
         <DictationQuestion />
         <DictationField />
       </div>
-    </div>
+    </DictationLayout>
   )
 }
