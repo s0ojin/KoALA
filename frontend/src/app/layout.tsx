60cf6c3d25972e83fe7a1170f01a047f1ce02ce1
s0ojin <onepst@hanyang.ac.kr>
2024-08-02 11:08:11 +0900

    Chore: 헤더 적용

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { SWRProvider } from './swr-provider'
+import Header from './_components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SWRProvider>{children}</SWRProvider>
+        <Header />
+        <SWRProvider>
+          <main>{children}</main>
+        </SWRProvider>
       </body>
     </html>
   )
