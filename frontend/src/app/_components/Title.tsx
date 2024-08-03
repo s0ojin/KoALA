ba5d92eff4ebe7aa63b7a5c89d84d42187bc5f33
s0ojin <onepst@hanyang.ac.kr>
2024-08-03 20:01:02 +0900

    Chore: Title파일 checkout

---
+++ frontend/src/app/_components/Title.tsx
@@ -0,0 +1,17 @@
+interface TitleProps {
+  CourseTitle: String
+  CourseDescription: String
+}
+
+export default function Title({ CourseTitle, CourseDescription }: TitleProps) {
+  return (
+    <header className="mt-28">
+      <h1 className="text-center text-3xl text-gray-800 font-bold">
+        {CourseTitle}
+      </h1>
+      <p className="text-center mt-2 text-xl text-gray-800">
+        {CourseDescription}
+      </p>
+    </header>
+  )
+}
