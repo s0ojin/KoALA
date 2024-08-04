6e8d36bd9a3bc36c475a20d97beb1cce30ffa486
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-08-05 00:57:13 +0900

    Feat: Chart 설계

--- frontend/src/app/report/page.tsx
+++ frontend/src/app/report/page.tsx
@@ -1,10 +1,11 @@
 import ReportLayout from "@/app/report/_components/ReportLayout"
+import ReportWeekChart from "@/app/report/_components/ReportWeekChart"
 
 export default function ReportMain () {
   return (
     <ReportLayout>
       <div className="relative bg-white w-[80%] h-auto min-h-main-height mx-auto mt-11 p-8 z-10 rounded-2xl">
-        
+        <ReportWeekChart />
       </div>
     </ReportLayout>
   )
