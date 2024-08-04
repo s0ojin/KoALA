cef346b3226e626ce233fc2c033546bbc454c81a
tjdgus2308 <tjdgus2308@gmail.com>
2024-08-04 14:20:19 +0900

    Feat: 레포트 영역 설정

--- frontend/src/app/report/page.tsx
+++ frontend/src/app/report/page.tsx
@@ -3,7 +3,9 @@ import ReportLayout from "@/app/report/_components/ReportLayout"
 export default function ReportMain () {
   return (
     <ReportLayout>
-      
+      <div className="relative bg-white w-[80%] h-auto min-h-main-height mx-auto mt-11 p-8 z-10 rounded-2xl">
+        
+      </div>
     </ReportLayout>
   )
 }
