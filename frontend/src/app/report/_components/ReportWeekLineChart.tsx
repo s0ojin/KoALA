f75ebc5d25fe762b916a3e9b36612722fe2f7188
tjdvjd1160 <tjdvjd1160@gmail.com>
2024-08-06 00:48:15 +0900

    Feat: Bar 차트, Line 차트 구현

--- frontend/src/app/report/_components/ReportWeekLineChart.tsx
+++ frontend/src/app/report/_components/ReportWeekLineChart.tsx
@@ -63,10 +63,9 @@ const data:ChartData <'line', number[]> = {
   labels,
   datasets: [
     {
-      label: '지난 주', //그래프 분류되는 항목
-      data: [4, 3, 3, 1, 7, 6, 3], //실제 그려지는 데이터(Y축 숫자)
-      borderColor: '#F6C4E8', //그래프 선 color
-      // backgroundColor: 'rgba(255, 99, 132, 0.5)', //마우스 호버시 나타나는 분류네모 표시 bg
+      label: '지난 주',
+      data: [4, 3, 3, 1, 7, 6, 3],
+      borderColor: '#F6C4E8',
       cubicInterpolationMode: 'monotone',
       pointBackgroundColor:'#F162CA',
       pointBorderColor:'#F162CA',
@@ -75,8 +74,7 @@ const data:ChartData <'line', number[]> = {
     {
       label: '이번 주',
       data: [3, 2, 2, 1, 6, 5, 2],
-      borderColor: '#DAD8FF', //실제 그려지는 데이터(Y축 숫자)
-      // backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      borderColor: '#DAD8FF',
       cubicInterpolationMode: 'monotone',
       pointBackgroundColor:'#4F46F2',
       pointBorderColor:'#4F46F2',
@@ -87,11 +85,11 @@ const data:ChartData <'line', number[]> = {
 
 export default function ReportWeekLineChart() {
   return (
-    <div className='contentWrap'>
-      <div className='flex'>
+    <div className='px-11 py-6'>
+      <div className='flex mb-8'>
         <div className='inline-flex items-center mr-8'>
           <div className='w-[3rem] h-[1rem] mr-1.5 bg-pink-400 rounded-2xl my-auto'/>
-          <p className='text-primary-400 text-pink-400'>
+          <p className='text-pink-400'>
             7/8 ~ 7/14
           </p>
         </div>
@@ -102,7 +100,7 @@ export default function ReportWeekLineChart() {
           </p>
         </div>
       </div>
-      <div className='w-full'>
+      <div>
         <Line options={options} data={data} />
       </div>
     </div>
