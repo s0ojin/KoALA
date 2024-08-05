4f28ed4d7af244a9d4ab6fdc1493581bf039a25a
LeeSpect <wewqwew153@gmail.com>
2024-08-05 12:39:44 +0900

    Feat: StudyTimeService에 회원가입하면 회원의 공부시간 초기화해서 저장하는 메서드 추가

--- backend/koala_back/src/main/java/com/ssafy/domain/user/service/StudyTimeServiceImpl.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/service/StudyTimeServiceImpl.java
@@ -3,6 +3,9 @@ package com.ssafy.domain.user.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.domain.user.model.entity.StudyTime;
+import com.ssafy.domain.user.repository.StudyTimeRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -10,8 +13,20 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class StudyTimeServiceImpl implements StudyTimeService {
 
+	private final StudyTimeRepository studyTimeRepository;
+
 	@Override
+	@Transactional
 	public void initStudyTime(Long userId) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < 3; i++) {
+			StudyTime studyTime = StudyTime.builder()
+				.timeCalType(i)
+				.userId(userId)
+				.talkTime(0)
+				.sentenceNum(0)
+				.lectureNum(0)
+				.build();
+			studyTimeRepository.save(studyTime);
+		}
 	}
 }
