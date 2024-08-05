497482eb789a7659eab0130913edabe5f7f4e361
dongyeon <uuas5866@naver.com>
2024-08-05 12:48:52 +0900

    Refactor: 이미지에서 문장 추출 기능

--- backend/koala_back/src/main/java/com/ssafy/domain/image/controller/ImageController.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/image/controller/ImageController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.domain.image.model.dto.response.TextResponse;
 import com.ssafy.domain.image.service.ImageService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,7 +26,13 @@ public class ImageController {
 
 	@PostMapping
 	public ResponseEntity<?> imageToText(@RequestParam("file") MultipartFile file) throws IOException {
-		String extractedText = imageService.imageToText(file);
-		return ResponseEntity.status(HttpStatus.CREATED).body(extractedText);
+		String response = imageService.imageToText(file);
+		// to. 윤서영...
+		// 파싱을 못하는 못난 웅니라서 미아내... 흑흑
+		// imageToText에서 String으로 문장을 주는거야!!
+		// 그럼 맞춤법 검사기 돌려서 json 객체로 바꿔서 보내면 됑.
+		// 일단 response 객체는 TextResponse로 만들어두었습니다.
+		// 쪽....
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
--- backend/koala_back/src/main/java/com/ssafy/domain/image/service/ImageService.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/image/service/ImageService.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.domain.image.model.dto.response.TextResponse;
+
 public interface ImageService {
 
 	String imageToText(MultipartFile multipartFile) throws IOException;
