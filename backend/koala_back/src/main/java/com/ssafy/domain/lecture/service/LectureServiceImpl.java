cb96f24eaaa4e26e853e269c90f30b57aa766879
ssyoung102 <ssyoung102@gmail.com>
2024-08-05 16:34:08 +0900

    Feat: 강의, 강의노트, 세션 기능

--- backend/koala_back/src/main/java/com/ssafy/domain/lecture/service/LectureServiceImpl.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/lecture/service/LectureServiceImpl.java
@@ -1,13 +1,22 @@
 package com.ssafy.domain.lecture.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.domain.lecture.model.dto.request.LectureNoteRequest;
+import com.ssafy.domain.lecture.model.dto.response.LectureNoteResponse;
 import com.ssafy.domain.lecture.model.dto.response.LectureResponse;
+import com.ssafy.domain.lecture.model.dto.response.RegisteredLectureResponse;
 import com.ssafy.domain.lecture.model.entity.Lecture;
+import com.ssafy.domain.lecture.model.entity.RegisteredLecture;
+import com.ssafy.domain.lecture.model.entity.RegisteredLectureId;
+import com.ssafy.domain.lecture.repository.LectureNoteRepository;
 import com.ssafy.domain.lecture.repository.LectureRepository;
+import com.ssafy.domain.lecture.repository.RegisteredLectureRepository;
+import com.ssafy.global.common.UserInfoProvider;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,15 +26,83 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class LectureServiceImpl implements LectureService {
 	private final LectureRepository lectureRepository;
+	private final UserInfoProvider userInfoProvider;
+	private final LectureNoteRepository lectureNoteRepository;
+	private final RegisteredLectureRepository registeredLectureRepository;
 
 	@Override
-	public List<LectureResponse> getAllLectures() {
+	public List<LectureResponse> getAllLecture() {
 		List<Lecture> lectures = lectureRepository.findAll();
 		return lectures.stream().map(LectureResponse::toDto).collect(Collectors.toList());
 	}
 
 	@Override
-	public void writeLectureNote(Long lectureId) {
+	public RegisteredLectureResponse registerLecture(Long lectureId) {
+		Optional<Lecture> lecture = lectureRepository.findById(lectureId);
+		if (lecture.isPresent()) {
+			RegisteredLecture registeredLecture = new RegisteredLecture();
 
+			registeredLecture.setUser(userInfoProvider.getCurrentUser());
+			registeredLecture.setLecture(lecture.get());
+			registeredLecture.setId(new RegisteredLectureId(userInfoProvider.getCurrentUserId(), lectureId));
+
+			registeredLectureRepository.save(registeredLecture);
+			return RegisteredLectureResponse.toDto(registeredLecture);
+		}
+		return null;
+	}
+
+	@Override
+	public List<RegisteredLectureResponse> getRegisteredLecture() {
+		return registeredLectureRepository.findByUserId(userInfoProvider.getCurrentUserId())
+			.stream()
+			.map(RegisteredLectureResponse::toDto)
+			.collect(Collectors.toList());
+	}
+
+	@Override
+	public LectureNoteResponse writeLectureNote(LectureNoteRequest lectureNoteRequest) {
+		Optional<Lecture> lecture = lectureRepository.findById(lectureNoteRequest.getLectureId());
+		return lecture.map(value -> LectureNoteResponse.toDto(
+				lectureNoteRepository.save(lectureNoteRequest.toEntity(userInfoProvider.getCurrentUser(), value))))
+			.orElse(null);
+	}
+
+	@Override
+	public List<LectureNoteResponse> readLectureNote(Long lectureId) {
+		return lectureNoteRepository.findById(lectureId)
+			.stream()
+			.map(LectureNoteResponse::toDto)
+			.collect(Collectors.toList());
+	}
+
+	@Override
+	public boolean deleteLectureNote(Long lectureId) {
+		if (lectureNoteRepository.existsById(lectureId)) {
+			lectureNoteRepository.deleteById(lectureId);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public LectureResponse setSessionId(Long lectureId, String sessionId) {
+		Optional<Lecture> lecture = lectureRepository.findById(lectureId);
+		if (lecture.isPresent()) {
+			lecture.get().setSessionId(sessionId);
+			return LectureResponse.toDto(lectureRepository.save(lecture.get()));
+		}
+		return null;
+	}
+
+	@Override
+	public String getSessionId(Long lectureId) {
+		Optional<Lecture> lecture = lectureRepository.findById(lectureId);
+		if (lecture.isPresent()) {
+			return lecture.get().getSessionId();
+		} else {
+			return null;
+		}
 	}
 }
