0bbaef1918b4d410e8d2b0a2a7dbaf8d97ef7f42
ssyoung102 <ssyoung102@gmail.com>
2024-08-05 16:33:08 +0900

    FIx: session Id 반환

--- backend/koala_back/src/main/java/com/ssafy/domain/lecture/model/dto/response/LectureResponse.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/lecture/model/dto/response/LectureResponse.java
@@ -1,8 +1,7 @@
 package com.ssafy.domain.lecture.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.domain.lecture.model.entity.Lecture;
-import com.ssafy.domain.user.model.dto.response.UserResponse;
-import com.ssafy.domain.user.model.entity.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,21 +13,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class LectureResponse {
+	@JsonProperty("lecture_id")
 	private Long lectureId;
-	private Long teacherId;
+
+	@JsonProperty("teacher_name")
+	private String teacherName;
+
+	@JsonProperty("lecture_title")
 	private String lectureTitle;
+
+	@JsonProperty("lecture_detail")
 	private String lectureDetail;
-	private String lectureUrl;
-	private boolean isOpen;
+
+	@JsonProperty("session_id")
+	private String sessionId;
 
 	public static LectureResponse toDto(Lecture lecture) {
 		return LectureResponse.builder()
 			.lectureId(lecture.getLectureId())
-			.teacherId(lecture.getTeacher().getUserId())
+			.teacherName(lecture.getTeacher().getUsername())
 			.lectureTitle(lecture.getLectureTitle())
 			.lectureDetail(lecture.getLectureDetail())
-			.lectureUrl(lecture.getLectureUrl())
-			.isOpen(lecture.getIsOpen() != 1)
+			.sessionId(lecture.getSessionId())
 			.build();
 	}
+
 }
