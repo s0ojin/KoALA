28c903979a6994539cbf65e3c5758872a353cad4
ssyoung102 <ssyoung102@gmail.com>
2024-08-05 16:34:22 +0900

    Fix: 세션 DB 참조

--- backend/koala_back/src/main/java/com/ssafy/domain/lecture/controller/LectureSessionController.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/lecture/controller/LectureSessionController.java
@@ -2,7 +2,6 @@ package com.ssafy.domain.lecture.controller;
 
 import java.util.Map;
 
-import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.domain.lecture.model.dto.response.RegisteredLectureResponse;
+import com.ssafy.domain.lecture.service.LectureService;
 
 import io.openvidu.java.client.Connection;
 import io.openvidu.java.client.ConnectionProperties;
@@ -26,44 +28,51 @@ import lombok.extern.slf4j.Slf4j;
 @CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/lectures/openvidu")
-public class OpenViduController {
+@RequestMapping("/lectures")
+public class LectureSessionController {
 
 	private final OpenVidu openvidu;
+	private final LectureService lectureService;
 
-	@PostMapping("/sessions")
-	public ResponseEntity<?> initializeSession(@RequestBody(required = false) Map<String, Object> params) {
+	@PostMapping("/{lectureId}/make-session")
+	public ResponseEntity<?> initializeSession(@PathVariable("lectureId") Long lectureId,
+		@RequestBody(required = false) Map<String, Object> params) {
 		try {
 			SessionProperties properties = SessionProperties.fromJson(params).build();
 			Session session = openvidu.createSession(properties);
-			JSONObject response = new JSONObject();
-			response.put("sessionId", session.getSessionId());
-			return ResponseEntity.status(HttpStatus.CREATED).body(response.toString());
+			// 해당 강의에 세션 아이디 추가
+			lectureService.setSessionId(lectureId, session.getSessionId());
+			// 수강하는 강의에 추가: 강의 노트 추가할 때 추가하고 싶으면 lecture controller 이동
+			RegisteredLectureResponse registeredLecture = lectureService.registerLecture(lectureId);
+			return ResponseEntity.status(HttpStatus.CREATED)
+				.body(Map.of("session_id", session.getSessionId(), "lecture_id", registeredLecture.getLectureId()));
 		} catch (OpenViduJavaClientException | OpenViduHttpException e) {
 			log.error("Error creating session", e);
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-				.body(new JSONObject().put("message", "Error creating session").toString());
+				.body(Map.of("message", "Error creating session"));
 		}
 	}
 
-	@PostMapping("/sessions/{sessionId}/connections")
-	public ResponseEntity<?> createConnection(@PathVariable("sessionId") String sessionId,
+	@PostMapping("/{lectureId}/connections")
+	public ResponseEntity<?> createConnection(@PathVariable("lectureId") Long lectureId,
 		@RequestBody(required = false) Map<String, Object> params) {
 		try {
+			String sessionId = lectureService.getSessionId(lectureId);
+			if (sessionId == null) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "Lecture not found"));
+			}
 			Session session = openvidu.getActiveSession(sessionId);
 			if (session == null) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-					.body(new JSONObject().put("message", "Session not found").toString());
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "Session not found"));
 			}
 			ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
 			Connection connection = session.createConnection(properties);
-			JSONObject response = new JSONObject();
-			response.put("token", connection.getToken());
-			return ResponseEntity.status(HttpStatus.CREATED).body(response.toString());
+			// lectureService.registerd
+			return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("token", connection.getToken()));
 		} catch (OpenViduJavaClientException | OpenViduHttpException e) {
 			log.error("Error creating connection", e);
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-				.body(new JSONObject().put("message", "Error creating connection").toString());
+				.body(Map.of("message", "Error creating connection"));
 		}
 	}
 }
