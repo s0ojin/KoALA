cade409dbf2bb96851dd6cdc0e0e2ae497925f55
tjdgus2308 <tjdgus2308@gmail.com>
2024-08-05 18:37:30 +0900

    Test: 차트 컴포넌트 테스트

--- frontend/src/app/report/page.tsx
+++ frontend/src/app/report/page.tsx
@@ -1,11 +1,15 @@
-import ReportLayout from "@/app/report/_components/ReportLayout"
-import ReportWeekChart from "@/app/report/_components/ReportWeekChart"
+import ReportLayout from '@/app/report/_components/ReportLayout'
+import ReportWeekLineChart from '@/app/report/_components/ReportWeekLineChart'
+import ReportFieldBarChart from '@/app/report/_components/ReportFieldBarChart'
+import ReportFieldPieChart from '@/app/report/_components/ReportFieldPieChart'
 
-export default function ReportMain () {
+export default function ReportMain() {
   return (
     <ReportLayout>
       <div className="relative bg-white w-[80%] h-auto min-h-main-height mx-auto mt-11 p-8 z-10 rounded-2xl">
-        <ReportWeekChart />
+        <ReportWeekLineChart />
+        <ReportFieldBarChart />
+        <ReportFieldPieChart />
       </div>
     </ReportLayout>
   )
