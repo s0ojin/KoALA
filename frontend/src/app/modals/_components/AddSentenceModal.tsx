4f790dc4320ed272c96ed72e2636bcae9688ad6b
s0ojin <onepst@hanyang.ac.kr>
2024-08-06 12:47:18 +0900

    Design: 모달 위치 변경

--- frontend/src/app/modals/_components/AddSentenceModal.tsx
+++ frontend/src/app/modals/_components/AddSentenceModal.tsx
@@ -76,35 +76,33 @@ export default function AddSentenceModal() {
   const [status, setStatus] = useState('initial')
 
   return (
-    <div className="absolute right-24 top-20">
-      <div className="bg-white max-w-[60rem] min-w-80 max-h-[44rem] rounded-3xl p-12 flex flex-col">
-        {status === 'initial' ? (
-          <div className="bg-primary-400 rounded-full py-4 px-6 text-center text-white mx-auto flex gap-3 items-center shadow-md">
-            <UploadIcon className="w-8" />
-            추가할 문장을 업로드 해주세요.
+    <div className="bg-white max-w-[60rem] min-w-80 max-h-[44rem] rounded-3xl p-12 flex flex-col">
+      {status === 'initial' ? (
+        <div className="bg-primary-400 rounded-full py-4 px-6 text-center text-white mx-auto flex gap-3 items-center shadow-md">
+          <UploadIcon className="w-8" />
+          추가할 문장을 업로드 해주세요.
+        </div>
+      ) : status === 'loading' ? (
+        <div>분석중입니다....</div>
+      ) : (
+        <>
+          <h1 className="text-2xl text-gray-900">
+            학습을 원하는 문장을 추가해주세요.
+          </h1>
+          <hr className="w-full my-4" />
+          <div className="h-full overflow-auto">
+            <ul className="flex flex-col gap-3 pr-3">
+              {dummy.map((sentence) => (
+                <SentenceAddCard
+                  key={sentence.sentence_id}
+                  sentenceId={sentence.sentence_id}
+                  sentence={sentence.sentence_text}
+                />
+              ))}
+            </ul>
           </div>
-        ) : status === 'loading' ? (
-          <div>분석중입니다....</div>
-        ) : (
-          <>
-            <h1 className="text-2xl text-gray-900">
-              학습을 원하는 문장을 추가해주세요.
-            </h1>
-            <hr className="w-full my-4" />
-            <div className="h-full overflow-auto">
-              <ul className="flex flex-col gap-3 pr-3">
-                {dummy.map((sentence) => (
-                  <SentenceAddCard
-                    key={sentence.sentence_id}
-                    sentenceId={sentence.sentence_id}
-                    sentence={sentence.sentence_text}
-                  />
-                ))}
-              </ul>
-            </div>
-          </>
-        )}
-      </div>
+        </>
+      )}
     </div>
   )
 }
