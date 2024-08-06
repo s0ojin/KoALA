901e0237b3d0f606029df03d9374a312de79cba4
LeeSpect <wewqwew153@gmail.com>
2024-08-06 15:22:09 +0900

    Feat: AiTalkLog entity 구현

---
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/model/entity/AiTalkLog.java
@@ -0,0 +1,43 @@
+package com.ssafy.domain.user.model.entity;
+
+import static jakarta.persistence.FetchType.*;
+import static lombok.AccessLevel.*;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "ai_talk_logs")
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+public class AiTalkLog {
+
+	@Id
+	@Column(name = "user_id")
+	private Long userId;
+
+	@MapsId
+	@OneToOne(fetch = LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(name = "start_time")
+	private LocalDateTime startTime;
+
+	@Column(name = "end_time")
+	private LocalDateTime endTime;
+
+}
