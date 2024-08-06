a46e86200b6abb7950dda9cac331966fab30d6e5
LeeSpect <wewqwew153@gmail.com>
2024-08-06 14:35:05 +0900

    Refactor: glabal에 자바 컨벤션 적용

--- backend/koala_back/src/main/java/com/ssafy/global/auth/jwt/dto/JwtToken.java
+++ backend/koala_back/src/main/java/com/ssafy/global/auth/jwt/dto/JwtToken.java
@@ -8,10 +8,14 @@ import lombok.Getter;
 @Getter
 @Builder
 public class JwtToken {
+
     @JsonProperty("grant_type")
     private String grantType; //  Bearer
+
     @JsonProperty("access_token")
     private String accessToken;
+
     @JsonProperty("refresh_token")
     private String refreshToken;
+
 }
