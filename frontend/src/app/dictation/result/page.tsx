24c522a58c2e00c13b33c608a14b32ed172cb849
박수진 <one_pst@naver.com>
2024-08-06 13:51:48 +0900

    Design: online-learning 레이아웃수정

--- frontend/src/app/dictation/result/page.tsx
+++ frontend/src/app/dictation/result/page.tsx
@@ -5,7 +5,7 @@ import Replay from '/public/icons/re-button.svg'
 export default function DictationResult() {
   return (
     <DictationResultLayout>
-      <div className="gap-20 pt-44 pb-40 w-full h-full min-h-screen flex items-center flex-col">
+      <div className="gap-20 pt-44 pb-40 w-full h-full min-h-main-screen flex items-center flex-col">
         <GradingSheet />
         <div className="flex flex-col gap-12 items-center">
           <button className="px-48 text-2xl font-medium text-white gap-2 items-center rounded-full py-4 bg-primary-400 flex">
