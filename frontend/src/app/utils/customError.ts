e4e43b9aee021372e4c81112097a050411d902e8
yukyung <kyg0954@naver.com>
2024-08-07 18:04:40 +0900

    Feat: customError class 제작

---
+++ frontend/src/app/utils/customError.ts
@@ -0,0 +1,9 @@
+export class ApiError extends Error {
+  public status: number
+
+  constructor(status: number, message: string) {
+    super(message)
+    this.status = status
+    this.name = 'ApiError'
+  }
+}
