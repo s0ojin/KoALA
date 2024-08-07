b222f4d5c1dfe7c190f9efc04f8141c2ea2411aa
ssyoung102 <ssyoung102@gmail.com>
2024-08-07 16:29:45 +0900

    Fix: CORS 오류

--- backend/koala_back/src/main/java/com/ssafy/global/config/CorsConfig.java
+++ backend/koala_back/src/main/java/com/ssafy/global/config/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
@@ -14,9 +15,10 @@ public class CorsConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of("*"));
+		// config.setAllowedOrigins(List.of("http://localhost:3000"));
+		// TODO: 나중에 프론트 url이 정확히 정해지면 제한 두는게 좋을 것 같습니다 ^^
+		config.setAllowedOriginPatterns(List.of("*"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setExposedHeaders(List.of("*"));
@@ -24,6 +26,11 @@ public class CorsConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
 		return source;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		return new CorsFilter(corsConfigurationSource());
 	}
 
 }
