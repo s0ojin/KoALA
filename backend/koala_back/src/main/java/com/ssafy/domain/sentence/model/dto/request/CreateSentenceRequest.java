af93d453c1e74d90bf6f5b7c902b0fd6ec24fb19
LeeSpect <wewqwew153@gmail.com>
2024-08-08 14:45:52 +0900

    Feat: Create Sentence Request에 toEntity 메서드 추가

--- backend/koala_back/src/main/java/com/ssafy/domain/sentence/model/dto/request/CreateSentenceRequest.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/sentence/model/dto/request/CreateSentenceRequest.java
@@ -1,6 +1,8 @@
 package com.ssafy.domain.sentence.model.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.domain.sentence.model.entity.Sentence;
+import com.ssafy.domain.user.model.entity.User;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -11,5 +13,14 @@ public class CreateSentenceRequest {
 	@JsonProperty("sentence_text")
 	@NotBlank(message = "문장은 필수 입력 값입니다.")
 	private String sentenceText;
+
+	public Sentence toEntity(User user) {
+		return Sentence.builder()
+			.sentenceText(sentenceText)
+			.topicCategory("사용자")
+			.sentenceLength(sentenceText.length())
+			.user(user)
+			.build();
+	}
 
 }
