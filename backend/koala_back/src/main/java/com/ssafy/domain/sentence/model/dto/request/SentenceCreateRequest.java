7475e1900e8eae828a097584ab3cd2587d3a3a61
LeeSpect <wewqwew153@gmail.com>
2024-08-08 15:05:36 +0900

    Refactor: Sentence Create Request로 데이터를 받을 때 글자 수 36자 유효성 검사 코드 추가

--- backend/koala_back/src/main/java/com/ssafy/domain/sentence/model/dto/request/SentenceCreateRequest.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/sentence/model/dto/request/SentenceCreateRequest.java
@@ -5,6 +5,7 @@ import com.ssafy.domain.sentence.model.entity.Sentence;
 import com.ssafy.domain.user.model.entity.User;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -12,6 +13,7 @@ public class SentenceCreateRequest {
 
 	@JsonProperty("sentence_text")
 	@NotBlank(message = "문장은 필수 입력 값입니다.")
+	@Size(max = 36, message = "문장은 36자 이하로 입력해주세요.")
 	private String sentenceText;
 
 	public Sentence toEntity(User user) {
