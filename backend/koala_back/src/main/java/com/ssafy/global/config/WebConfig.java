86f1948e581373ae3604f699bd569d1635994892
ssyoung102 <ssyoung102@gmail.com>
2024-08-08 10:45:17 +0900

    Fix: CORS 오류

--- backend/koala_back/src/main/java/com/ssafy/global/config/WebConfig.java
+++ backend/koala_back/src/main/java/com/ssafy/global/config/WebConfig.java
@@ -13,13 +13,4 @@ public class WebConfig implements WebMvcConfigurer {
 		configurer.addPathPrefix("api", c -> true);
 	}
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-			.allowedHeaders("*")
-			.allowCredentials(true);
-	}
-
 }
