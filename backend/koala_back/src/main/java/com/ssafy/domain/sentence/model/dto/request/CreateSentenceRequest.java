b4b274f07d04c0a84f4c37c27a970d48b4b78571
LeeSpect <wewqwew153@gmail.com>
2024-08-08 14:41:06 +0900

    Refactor: Create Sentence Request Dto 필드명 수정

--- backend/koala_back/src/main/java/com/ssafy/domain/sentence/model/dto/request/CreateSentenceRequest.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/sentence/model/dto/request/CreateSentenceRequest.java
@@ -1,12 +1,15 @@
 package com.ssafy.domain.sentence.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class CreateSentenceRequest {
 
+	@JsonProperty("sentence_text")
 	@NotBlank(message = "문장은 필수 입력 값입니다.")
-	private String sentence;
+	private String sentenceText;
 
 }
