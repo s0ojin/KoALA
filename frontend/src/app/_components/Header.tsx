03ce5875ba3b9b900d398bd0f93d7beb3220441b
sujin <one_pst@naver.com>
2024-08-08 21:25:53 +0900

    Feat: Header 로그인 상태 추가

--- frontend/src/app/_components/Header.tsx
+++ frontend/src/app/_components/Header.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
 import Logo from '/public/images/logo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
 import UserDropdownMenu from '@/app/_components/UserDropdownMenu'
+import OnlineLearningHeader from '@/app/online-learning/_components/OnlineLearningHeader'
+import { useEffect, useRef, useState } from 'react'
 import { usePathname } from 'next/navigation'
-import OnlineLearningHeader from '../online-learning/_components/OnlineLearningHeader'
-import path from 'path'
+import { getUserInfo } from '@/app/apis/auth'
+import useSWR from 'swr'
 
 export default function Header() {
   const [isLogin, setIsLogin] = useState(true)
+  const { data: userInfo } = useSWR('/users', getUserInfo)
   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false)
   const userMenuRef = useRef<HTMLDivElement>(null)
   const pathname = usePathname()
@@ -48,11 +50,11 @@ export default function Header() {
         <Logo height="27" />
       </Link>
       <div className="flex gap-6 items-center">
-        {isLogin ? (
+        {userInfo ? (
           <>
             <Link
               href="/modals/add-sentence"
-              className="text-base font-medium bg-primary-400 text-white py-2 px-11 rounded-full"
+              className="bg-primary-400 text-white py-2 px-11 rounded-full"
             >
               나만의 문장 추가
             </Link>
@@ -72,9 +74,11 @@ export default function Header() {
             )}
           </>
         ) : (
-          <button className="text-base font-medium bg-primary-400 text-white py-2 px-11 rounded-full">
-            로그인
-          </button>
+          <Link href="/login">
+            <button className="bg-primary-400 text-white py-2 px-10 rounded-full">
+              로그인
+            </button>
+          </Link>
         )}
       </div>
     </header>
