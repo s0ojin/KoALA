2d48d639f4e30bb9f0dd2170b4a5f8d29d05619d
LeeSpect <wewqwew153@gmail.com>
2024-08-09 09:40:52 +0900

    Refactor: 회원 정보 수정 필드를 닉네임으로 제한

--- backend/koala_back/src/main/java/com/ssafy/domain/user/model/dto/request/UserUpdateRequest.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/model/dto/request/UserUpdateRequest.java
@@ -11,10 +11,7 @@ public class UserUpdateRequest {
 	@NotBlank(message = "닉네임은 필수 입력 값입니다.")
 	String nickname;
 
-	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-	String password;
-
 	public User toEntity(String nickname, String encodedPassword) {
-		return User.builder().nickname(nickname).password(encodedPassword).build();
+		return User.builder().nickname(nickname).build();
 	}
 }
