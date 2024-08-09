54ee26777e6d8a1d55634fb0c45bad2057535e17
yukyung <kyg0954@naver.com>
2024-08-09 14:15:59 +0900

    Refactor: 코알라 메인페이지 레이아웃 수정

--- frontend/src/app/main/page.tsx
+++ frontend/src/app/main/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
-import Header from '@/app/_components/Header'
 import MainLayout from '@/app/main/_components/MainLayout'
 import EducationCardList from '@/app/main/_components/MainEducationCardList'
-import MainKoalaLevel from '@/app/main/_components/MainKoalaLevel'
 import EucalyptusButton from '@/app/main/_components/MainEucalyptusButton'
+import MainKoala from './_components/MainKoala'
+import MainCommunityButton from './_components/MainCommunityButton'
 
 export default function Main() {
   return (
@@ -11,24 +11,14 @@ export default function Main() {
       <div className="w-full h-auto min-h-main-height overflow-hidden relative select-none z-10 flex flex-col items-center">
         <div className="relative w-screen h-full min-h-main-height flex max-w-[94rem]">
           <div className=" w-2/5 h-1/2 min-h-[32rem] self-end ml-12 mb-8 relative flex items-end justify-center">
-            <div className="absolute top-0 left-0">
-              <EucalyptusButton eucalyptusCount={10} />
+            <div className="flex flex-col gap-6 absolute top-0 left-0 z-10">
+              <MainCommunityButton />
+              <EucalyptusButton eucalyptusCount={100} />
             </div>
-            <div className="flex flex-col items-center w-full">
-              <Image
-                src="/images/koala-sleep.png"
-                width={0}
-                height={0}
-                sizes="100%"
-                className="w-[22rem] h-[22rem]"
-                alt="koala"
-                draggable="false"
-              />
-              <MainKoalaLevel percent={50} />
-            </div>
+            <MainKoala />
           </div>
 
-          <div className="absolute right-0 top-1/3">
+          <div className="absolute -right-28 top-1/3">
             <EducationCardList />
           </div>
         </div>
