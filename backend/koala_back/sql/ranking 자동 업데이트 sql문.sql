b12fcf0be4241953dc50c0c09675c9c0175e4e0c
LeeSpect <wewqwew153@gmail.com>
2024-08-09 15:54:36 +0900

    Docs: sql문 수정

--- backend/koala_back/sql/ranking 자동 업데이트 sql문.sql
+++ backend/koala_back/sql/ranking 자동 업데이트 sql문.sql
@@ -1,5 +1,6 @@
 DROP PROCEDURE IF EXISTS UpdateUserRanking;
 DROP TRIGGER IF EXISTS UpdateRankingAfterUserExpChange;
+DROP EVENT IF EXISTS UpdateUserRankingEvent;
 
 -- 1. 저장 프로시저
 
@@ -8,26 +9,26 @@ DELIMITER $$
 CREATE PROCEDURE UpdateUserRanking()
 BEGIN
     DECLARE prev_exp INT DEFAULT -1;
-    DECLARE rank INT DEFAULT 0;
-    DECLARE dense_rank INT DEFAULT 0;
+    DECLARE user_rank INT DEFAULT 0;
+    DECLARE current_dense_rank INT DEFAULT 0;
 
     -- 순위 테이블 초기화
-    TRUNCATE TABLE ranking;
+TRUNCATE TABLE ranking;
 
-    -- 사용자 경험치를 기준으로 순위를 매기기
-    SET @dense_rank = 0;
-    SET @rank = 0;
+-- 사용자 경험치를 기준으로 순위를 매기기
+SET @current_dense_rank = 0;
+    SET @user_rank = 0;
     SET @prev_exp = -1;
 
-    INSERT INTO ranking (user_id, ranking)
-    SELECT user_id, 
-           CASE 
-               WHEN @prev_exp = user_exp THEN @rank
-               ELSE @dense_rank := @dense_rank + 1
-           END AS dense_rank,
-           @prev_exp := user_exp
-    FROM users
-    ORDER BY user_exp DESC;
+INSERT INTO ranking (user_id, ranking)
+SELECT user_id,
+       CASE
+           WHEN @prev_exp = user_exp THEN @user_rank
+           ELSE @current_dense_rank := @current_dense_rank + 1
+           END AS ranking,
+       @prev_exp := user_exp
+FROM users
+ORDER BY user_exp DESC;
 END $$
 
 DELIMITER ;
@@ -46,15 +47,16 @@ DO
 DELIMITER $$
 
 CREATE TRIGGER UpdateRankingAfterUserExpChange
-AFTER UPDATE ON users
-FOR EACH ROW
+    AFTER UPDATE ON users
+    FOR EACH ROW
 BEGIN
     IF OLD.user_exp != NEW.user_exp THEN
         CALL UpdateUserRanking();
-    END IF;
+END IF;
 END $$
 
 DELIMITER ;
+
 
 
 -- 절취선
