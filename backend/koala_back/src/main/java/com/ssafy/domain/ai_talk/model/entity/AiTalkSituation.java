2732bdde4b8bad6ff3ed8d799e34cc564792f788
ssyoung102 <ssyoung102@gmail.com>
2024-08-09 17:09:07 +0900

    Fix: @GeneratedValue(strategy = GenerationType.IDENTITY)

--- backend/koala_back/src/main/java/com/ssafy/domain/ai_talk/model/entity/AiTalkSituation.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/ai_talk/model/entity/AiTalkSituation.java
@@ -5,6 +5,7 @@ import static lombok.AccessLevel.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = PROTECTED)
 public class AiTalkSituation {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "situation_id")
 	private Long situationId;
 
