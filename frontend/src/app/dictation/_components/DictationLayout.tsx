a044187ad54f6c1846f5a7a2a812c778eb49e1a3
박수진 <one_pst@naver.com>
2024-08-09 13:11:53 +0900

    Design: 헤더 투명하게 변경

--- frontend/src/app/dictation/_components/DictationLayout.tsx
+++ frontend/src/app/dictation/_components/DictationLayout.tsx
@@ -6,7 +6,7 @@ export default function DictationLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="relative w-full h-main-screen flex items-center justify-center overflow-x-hidden">
+    <div className="relative w-full h-main-screen flex items-center justify-center">
       {children}
       <Image
         width={0}
