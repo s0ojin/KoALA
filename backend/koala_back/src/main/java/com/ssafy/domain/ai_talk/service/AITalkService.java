a214bffc4e34f1a70b19963c61ad0cac09d87cbb
ssyoung102 <ssyoung102@gmail.com>
2024-08-09 13:12:37 +0900

    Fix: 머지 충돌 해결

--- backend/koala_back/src/main/java/com/ssafy/domain/ai_talk/service/AITalkService.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/ai_talk/service/AITalkService.java
@@ -2,12 +2,13 @@ package com.ssafy.domain.ai_talk.service;
 
 import com.ssafy.domain.ai_talk.dto.request.AITalkRequest;
 import com.ssafy.domain.ai_talk.dto.request.AITalkSituationRequest;
+import com.ssafy.domain.ai_talk.dto.response.AITalkFinishResponse;
 import com.ssafy.domain.ai_talk.dto.response.AITalkResponse;
 
 public interface AITalkService {
-	AITalkResponse setSituation(AITalkSituationRequest AITalkSituationRequest);
+	AITalkResponse setSituation(AITalkSituationRequest aiTalkSituationRequest);
 
-	AITalkResponse getAIResponse(AITalkRequest AITalkRequest);
+	AITalkResponse getAIResponse(AITalkRequest aiTalkRequest);
 
-	void finishAIResponse();
+	AITalkFinishResponse finishAIResponse();
 }
