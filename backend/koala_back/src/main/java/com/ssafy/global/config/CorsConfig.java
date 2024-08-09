2c9adb078ac3d882c6e56e2e1f2e217b8ab39849
wewqwew153 <wewqwew153@gmail.com>
2024-08-09 22:47:56 +0900

    Refactor: CorsConfig에 @primary 추가

--- backend/koala_back/src/main/java/com/ssafy/global/config/CorsConfig.java
+++ backend/koala_back/src/main/java/com/ssafy/global/config/CorsConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,6 +14,7 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
 
 	@Bean
+	@Primary
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
