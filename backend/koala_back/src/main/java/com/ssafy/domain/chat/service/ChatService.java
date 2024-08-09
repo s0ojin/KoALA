b783f1a304d71f001e32d0ca3086b46262427df8
LeeSpect <wewqwew153@gmail.com>
2024-08-09 12:39:26 +0900

    Feat: AI 회화 종료 시 클라이언트 반환 값으로 유칼립투스 반환

--- backend/koala_back/src/main/java/com/ssafy/domain/chat/service/ChatService.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/chat/service/ChatService.java
@@ -2,6 +2,7 @@ package com.ssafy.domain.chat.service;
 
 import com.ssafy.domain.chat.dto.request.ChatRequest;
 import com.ssafy.domain.chat.dto.request.ChatSituationRequest;
+import com.ssafy.domain.chat.dto.response.ChatFinishResponse;
 import com.ssafy.domain.chat.dto.response.ChatResponse;
 
 public interface ChatService {
@@ -9,5 +10,5 @@ public interface ChatService {
 
 	ChatResponse getAIResponse(ChatRequest chatRequest);
 
-	void finishAIResponse();
+	ChatFinishResponse finishAIResponse();
 }
