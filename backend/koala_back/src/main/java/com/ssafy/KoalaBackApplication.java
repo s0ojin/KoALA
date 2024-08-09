2aa8285a9e59d51fcbbb4991c1fa5478e1f3c39d
ssyoung102 <ssyoung102@gmail.com>
2024-08-09 13:14:48 +0900

    Fix: 머지 충돌 해결

--- backend/koala_back/src/main/java/com/ssafy/KoalaBackApplication.java
+++ backend/koala_back/src/main/java/com/ssafy/KoalaBackApplication.java
@@ -14,7 +14,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class KoalaBackApplication {
 
 	public static void main(String[] args) {
-		System.out.println("반갑습니다");
 		SpringApplication.run(KoalaBackApplication.class, args);
 	}
 
