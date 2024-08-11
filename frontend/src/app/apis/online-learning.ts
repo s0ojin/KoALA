77682e1666009792b4747d2964c0a32c4ff1f22c
sujin <one_pst@naver.com>
2024-08-11 22:42:38 +0900

    Feat: 강의노트 조회 삭제 api연결

--- frontend/src/app/apis/online-learning.ts
+++ frontend/src/app/apis/online-learning.ts
@@ -6,6 +6,13 @@ interface CreateLectureNoteRequestBody {
   note_content: string
 }
 
+interface LectureNote {
+  note_id: number
+  note_title: string
+  note_content: string
+  note_created_at: string
+}
+
 export const postCreateOpenViduSession = async (url: string) => {
   try {
     const accessToken = getToken()
@@ -61,6 +68,52 @@ export const postCreateLectureNote = async (
         'Authorization': `Bearer ${accessToken}`,
       },
       body: JSON.stringify(payload),
+    })
+
+    if (!response.ok) {
+      console.log(response)
+    }
+    const data = await response.json()
+    return { data: data, status: response.status }
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error)
+  }
+}
+
+export const getLectureNoteList = async (
+  url: string
+): Promise<{ data: LectureNote[]; status: number } | undefined> => {
+  try {
+    const accessToken = getToken()
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    })
+
+    if (!response.ok) {
+      console.log(response)
+    }
+    const data = await response.json()
+    return { data: data, status: response.status }
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error)
+  }
+}
+
+export const deleteLectureNoteList = async (
+  url: string
+): Promise<{ data: LectureNote[]; status: number } | undefined> => {
+  try {
+    const accessToken = getToken()
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
     })
 
     if (!response.ok) {
