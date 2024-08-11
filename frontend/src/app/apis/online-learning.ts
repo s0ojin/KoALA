ccb82e23aaea8777a52559bc1076c26c1a83c91f
s0ojin <onepst@hanyang.ac.kr>
2024-08-11 21:34:43 +0900

    Feat: 강의노트생성 api연결

--- frontend/src/app/apis/online-learning.ts
+++ frontend/src/app/apis/online-learning.ts
@@ -1,5 +1,11 @@
 import { getToken } from '@/app/utils/cookie/getToken'
 
+interface CreateLectureNoteRequestBody {
+  lecture_id: number
+  note_title: string
+  note_content: string
+}
+
 export const postCreateOpenViduSession = async (url: string) => {
   try {
     const accessToken = getToken()
@@ -37,6 +43,31 @@ export const postCreateOpenViduConnection = async (url: string) => {
     }
     const data = await response.json()
     return { data: data.token, status: response.status }
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error)
+  }
+}
+
+export const postCreateLectureNote = async (
+  url: string,
+  payload: CreateLectureNoteRequestBody
+) => {
+  try {
+    const accessToken = getToken()
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(payload),
+    })
+
+    if (!response.ok) {
+      console.log(response)
+    }
+    const data = await response.json()
+    return { data: data, status: response.status }
   } catch (error) {
     console.error('There was a problem with the fetch operation:', error)
   }
