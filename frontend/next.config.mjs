19cc2fb5e97bf75c584948f9ebdb599a4c51e8f6
s0ojin <onepst@hanyang.ac.kr>
2024-08-13 18:34:09 +0900

    Feat: 화상채팅 메인 api연결

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   images: {
-    domains: ['koalabucket1.s3.amazonaws.com'],
+    domains: ['koalabucket1.s3.amazonaws.com', 'images.unsplash.com'],
   },
 
   output: 'standalone',
