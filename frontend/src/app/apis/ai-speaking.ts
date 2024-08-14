d06462730371ee8f9603312cfd8635be8ae73cb6
s0ojin <onepst@hanyang.ac.kr>
2024-08-15 01:10:40 +0900

    Design: ai회화 디자인 디테일 수정

--- frontend/src/app/apis/ai-speaking.ts
+++ frontend/src/app/apis/ai-speaking.ts
@@ -30,3 +30,76 @@ export const getAIConversationList = async (
     console.error('There was a problem with the fetch operation:', error)
   }
 }
+
+export const getStartAISpeaking = async (
+  url: string
+): Promise<{ data: { message: string }; status: number } | undefined> => {
+  try {
+    const accessToken = getToken()
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    })
+
+    if (!response.ok) {
+      console.log(response)
+    }
+    const data = await response.json()
+    return { data: data, status: response.status }
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error)
+  }
+}
+
+export const getEndAISpeaking = async (
+  url: string
+): Promise<
+  { data: { message: string; leaves: number }; status: number } | undefined
+> => {
+  try {
+    const accessToken = getToken()
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    })
+
+    if (!response.ok) {
+      console.log(response)
+    }
+    const data = await response.json()
+    return { data: data, status: response.status }
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error)
+  }
+}
+
+export const postSendAISpeakingMessage = async (
+  url: string,
+  payload: { message: string }
+): Promise<{ data: { message: string }; status: number } | undefined> => {
+  try {
+    const accessToken = getToken()
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(payload),
+    })
+
+    if (!response.ok) {
+      console.log(response)
+    }
+    const data = await response.json()
+    return { data: data, status: response.status }
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error)
+  }
+}
