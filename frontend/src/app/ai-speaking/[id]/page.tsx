c3669bffdebc56da0c11b38f7e77d19a138beeca
sujin <one_pst@naver.com>
2024-08-15 01:10:40 +0900

    Design: ai회화 디자인 디테일 수정

--- frontend/src/app/ai-speaking/[id]/page.tsx
+++ frontend/src/app/ai-speaking/[id]/page.tsx
@@ -4,7 +4,8 @@ import MicBtn from '/public/icons/microphone.svg'
 import SendBtn from '/public/icons/send.svg'
 import Image from 'next/image'
 import { useState } from 'react'
-import AISpeakingBackgroundLayout from '../_components/AISpeakingBackgroundLayout'
+import AISpeakingBackgroundLayout from '@/app/ai-speaking/_components/AISpeakingBackgroundLayout'
+import { useSearchParams } from 'next/navigation'
 
 const dummy = [
   {
@@ -18,7 +19,7 @@ const dummy = [
     isMine: false,
     message: '내가보낸 메세지',
     senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
+    senderProfile: '/images/koala-sleep.png',
     timeStamp: '2024-07-30T13:45:00+09:00',
   },
   {
@@ -30,165 +31,60 @@ const dummy = [
   {
     id: 2,
     isMine: false,
-    message: '내가보낸 메세지',
+    message:
+      '내가보낸 메세지내가보낸 메세지내가보낸 메세지내가보낸 메세지내가보낸 메세지내가보낸 메세지',
     senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 2,
-    isMine: false,
-    message: '내가보낸 메세지',
-    senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 2,
-    isMine: false,
-    message: '내가보낸 메세지',
-    senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 2,
-    isMine: false,
-    message: '내가보낸 메세지',
-    senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 2,
-    isMine: false,
-    message: '내가보낸 메세지',
-    senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 2,
-    isMine: false,
-    message: '내가보낸 메세지',
-    senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 2,
-    isMine: false,
-    message: '내가보낸 메세지',
-    senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 2,
-    isMine: false,
-    message: '내가보낸 메세지',
-    senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지dddddddddddddddddddd',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 1,
-    isMine: true,
-    message: '내가보낸 메세지',
-    timeStamp: '2024-07-30T13:45:00+09:00',
-  },
-  {
-    id: 2,
-    isMine: false,
-    message: '내가보낸 메세지',
-    senderName: '응 위엔',
-    senderProfile: '/public/images/koala-sleep.png',
+    senderProfile: '/images/koala-sleep.png',
     timeStamp: '2024-07-30T13:45:00+09:00',
   },
 ]
 
+type TopicKey = '일상' | '행정' | '교육'
+
+const BACKGROUND_DATA: Record<TopicKey, { bgImage: string; avatar: string }> = {
+  일상: {
+    bgImage: '/images/daily-background.jpg',
+    avatar: '/images/koala-front.png',
+  },
+  행정: {
+    bgImage: '/images/administration-background.jpg',
+    avatar: '/images/koala-business.png',
+  },
+  교육: {
+    bgImage: '/images/education-background.jpg',
+    avatar: '/images/koala-study.png',
+  },
+}
+
 export default function AISpeakingLearningRoom() {
   const [isSpeakMode, setIsSpeakMode] = useState(true)
+  const searchParams = useSearchParams()
+  const topic =
+    (searchParams.get('topic') as '일상' | '행정' | '교육') || '일상'
+
   return (
     <AISpeakingBackgroundLayout>
       <div className="h-main-screen flex items-center">
         <div className="h-[90%] w-[80%] max-w-7xl bg-white rounded-3xl mx-auto flex gap-10 p-8">
           <div className="relative bg-gray-300 rounded-tr-3xl overflow-hidden w-[50%] ">
-            <img
-              src="https://plus.unsplash.com/premium_photo-1680807869780-e0876a6f3cd5?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            <Image
+              src={BACKGROUND_DATA[topic].bgImage}
+              width={500}
+              height={500}
               alt="background"
               className="w-full h-full object-cover blur-sm"
+              priority
             />
             <Image
-              src="/images/koala-front.png"
+              src={BACKGROUND_DATA[topic].avatar}
               alt="koala-front"
               width={300}
               height={300}
-              className="absolute bottom-16 left-16"
+              className="absolute bottom-12 left-36"
             />
           </div>
-          <div className="w-[50%] flex flex-col gap-12 items-center">
-            <div className="w-full overflow-auto pr-3">
+          <div className="w-[50%] flex flex-col gap-12 items-center justify-between">
+            <div className="w-full overflow-auto pr-3 flex-1 flex flex-col gap-2">
               {dummy.map((chat) => (
                 <ChatBubble
                   key={chat.id}
@@ -200,7 +96,7 @@ export default function AISpeakingLearningRoom() {
                 />
               ))}
             </div>
-            <div className="flex-1 w-full text-center">
+            <div className="w-full text-center">
               {isSpeakMode ? (
                 <button>
                   <MicBtn className="w-16 text-primary-400" />
