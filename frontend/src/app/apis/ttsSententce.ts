84dcbde18674cf040d4006dc5a626cdfd4d6d228
tjdgus2308 <tjdgus2308@gmail.com>
2024-08-14 17:10:26 +0900

    Fix: 구글 TTS 요청 LocalStorage 활용

--- frontend/src/app/apis/ttsSententce.ts
+++ frontend/src/app/apis/ttsSententce.ts
@@ -1,47 +1,61 @@
 import { ApiError } from '@/app/utils/customError'
-import { getSpeech } from '@/app/apis/googleSentence';
+import { getSpeech } from '@/app/apis/googleSentence'
 
-export const getWebSpeech = (text:string, onStartCallback: () => void, onEndCallback: () => void) => {
-  let voices:SpeechSynthesisVoice[] = [];
+export const getWebSpeech = (
+  text: string,
+  onStartCallback: () => void,
+  onEndCallback: () => void
+) => {
+  let voices: SpeechSynthesisVoice[] = []
 
   const setVoiceList = () => {
-    voices = window.speechSynthesis.getVoices();
-  };
-
-  setVoiceList();
-
-  if (window.speechSynthesis.onvoiceschanged !== undefined) {
-    window.speechSynthesis.onvoiceschanged = setVoiceList;
+    voices = window.speechSynthesis.getVoices()
   }
 
-  const speech = (txt:string) => {
-    const lang = "ko-KR";
-    const utterThis = new SpeechSynthesisUtterance(txt);
-    utterThis.lang = lang;
+  setVoiceList()
+
+  if (window.speechSynthesis.onvoiceschanged !== undefined) {
+    window.speechSynthesis.onvoiceschanged = setVoiceList
+  }
+
+  const speech = (txt: string) => {
+    const lang = 'ko-KR'
+    const utterThis = new SpeechSynthesisUtterance(txt)
+    utterThis.lang = lang
     const kor_voice = voices.find(
-      (elem) => elem.lang === lang || elem.lang === lang.replace("-", "_")
-    );
+      (elem) => elem.lang === lang || elem.lang === lang.replace('-', '_')
+    )
     if (kor_voice) {
-      utterThis.voice = kor_voice;
+      utterThis.voice = kor_voice
     } else {
-      return;
+      return
     }
     utterThis.onstart = onStartCallback
     utterThis.onend = onEndCallback
-    window.speechSynthesis.speak(utterThis);
-  };
+    window.speechSynthesis.speak(utterThis)
+  }
 
-  speech(text);
-};
-
-export const stopWebSpeech = () => {
-    window.speechSynthesis.cancel()
+  speech(text)
 }
 
-export const getGoogleSpeech = async (text:string) => {
-  const response = await getSpeech(text)
+export const stopWebSpeech = () => {
+  window.speechSynthesis.cancel()
+}
+
+export const getGoogleSpeech = async (text: string) => {
+  let response = ''
+  const localData = localStorage.getItem(`${text}`)
+  if (!localData) {
+    response = await getSpeech(text)
+    localStorage.setItem(`${text}`, response)
+  } else {
+    response = localData
+  }
   const parsedres = await JSON.parse(response)
-  const buff = await Buffer.from(parsedres.blob,'base64')
-  const arraybuff = await buff.buffer.slice(buff.byteOffset, buff.byteOffset + buff.byteLength)
+  const buff = await Buffer.from(parsedres.blob, 'base64')
+  const arraybuff = await buff.buffer.slice(
+    buff.byteOffset,
+    buff.byteOffset + buff.byteLength
+  )
   return arraybuff
 }
