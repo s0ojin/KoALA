e39a4ef202bd34550bc2ac088fa8a0020b7369cf
KimYuKyung <kyg0954@naver.com>
2024-08-15 12:33:38 +0900

    Feat: 강의 노트 페이지 조회 API 연결

--- frontend/src/app/lecture-note/_components/LectureNoteElement.tsx
+++ frontend/src/app/lecture-note/_components/LectureNoteElement.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface LectureNoteListElementProps {
   lectureId: number
   teacherName: string
   lectureTitle: string
   lectureNoteCounts: number
+  lectureImgUrl: string
 }
 
 export default function LectureNoteElement({
@@ -12,24 +14,35 @@ export default function LectureNoteElement({
   teacherName,
   lectureTitle,
   lectureNoteCounts,
+  lectureImgUrl,
 }: LectureNoteListElementProps) {
   return (
     <div className="border-b-[1px] border-gray-200 bg-white w-full flex py-5 px-8 items-center">
       <div className="flex flex-col w-full gap-2">
-        <p className="text-xl font-medium">{lectureTitle}</p>
+        <Link
+          href={{
+            pathname: `/lecture-note/${lectureId}`,
+            query: {
+              lectureTitle,
+              lectureTeacher: teacherName,
+            },
+          }}
+        >
+          <p className="text-xl font-medium">{lectureTitle}</p>
+        </Link>
         <div className="flex items-center gap-3 text-base text-primary-900">
           <p>노트수 {lectureNoteCounts}개</p>
           <hr className="w-[1px] bg-primary-900 h-3" />
-          <p>{teacherName}</p>
+          <p>{teacherName} 선생님</p>
         </div>
       </div>
       <Image
-        src="/images/koala-sleep.png"
-        width={0}
-        height={0}
-        alt="teacher"
-        sizes="100%"
-        className="w-40 h-20 border rounded-lg"
+        src={lectureImgUrl}
+        width={160}
+        height={80}
+        alt="lecture"
+        className="w-40 h-20 border rounded-lg aspect-video"
+        priority
       />
     </div>
   )
--- frontend/src/app/lecture-note/page.tsx
+++ frontend/src/app/lecture-note/page.tsx
@@ -1,47 +1,25 @@
+import { getLectureList } from '../apis/lecture'
 import LectureNoteElement from './_components/LectureNoteElement'
 import LectureNoteLayout from './_components/LectureNoteLayout'
 
-const lectureNoteList = [
-  {
-    lecture_id: 1,
-    teacher_name: '김성현 선생님',
-    lecture_title: '한글의 이해',
-    note_counts: 23,
-  },
-  {
-    lecture_id: 2,
-    teacher_name: '김성현 선생님',
-    lecture_title: '한글의 이해',
-    note_counts: 20,
-  },
-  {
-    lecture_id: 3,
-    teacher_name: '김성현 선생님',
-    lecture_title: '한글의 이해',
-    note_counts: 5,
-  },
-  {
-    lecture_id: 4,
-    teacher_name: '김성현 선생님',
-    lecture_title: '한글의 이해',
-    note_counts: 58,
-  },
-]
+export default async function LectureNoteMain() {
+  const lectureListData = await getLectureList(`/lectures`)
+  const { lecture_list: lectureList } = lectureListData
 
-export default function LectureNoteMain() {
   return (
     <LectureNoteLayout>
-      <div className="flex flex-col items-center gap-7 h-full pt-20">
+      <div className="flex flex-col items-center gap-7 h-full pt-20 min-h-screen">
         <h1 className="text-3xl font-medium text-primary-900">강의 노트</h1>
         <div className="h-auto bg-white pt-10 pb-36 px-16 w-full max-w-[65rem] rounded-t-[4rem] flex-grow">
-          {lectureNoteList.map((lectureNote) => {
+          {lectureList?.map((lectureNote) => {
             return (
               <LectureNoteElement
                 key={lectureNote.lecture_id}
                 lectureId={lectureNote.lecture_id}
                 teacherName={lectureNote.teacher_name}
                 lectureTitle={lectureNote.lecture_title}
-                lectureNoteCounts={lectureNote.note_counts}
+                lectureNoteCounts={lectureNote.lecture_note_count}
+                lectureImgUrl={lectureNote.lecture_img_url}
               />
             )
           })}
